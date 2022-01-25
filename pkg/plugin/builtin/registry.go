8f2cd906e03bfcb60f85bcf2c4bda61d78057532
mahahajja <maha@meroxa.io>
2022-01-25 11:46:04 +0000

    build fix

--- pkg/plugin/builtin/registry.go
+++ pkg/plugin/builtin/registry.go
@@ -20,7 +20,6 @@ import (
 	"github.com/conduitio/conduit/pkg/plugin"
 	builtinv1 "github.com/conduitio/conduit/pkg/plugin/builtin/v1"
 	"github.com/conduitio/conduit/pkg/plugin/sdk"
-	"github.com/conduitio/conduit/pkg/plugins/file"
 	"github.com/conduitio/conduit/pkg/plugins/generator"
 	"github.com/conduitio/conduit/pkg/plugins/s3"
 	s3destination "github.com/conduitio/conduit/pkg/plugins/s3/destination"
@@ -29,7 +28,6 @@ import (
 
 var DefaultDispenserFactories = []DispenserFactory{
 	sdkDispenserFactory(generator.Specification, generator.NewSource, nil),
-	sdkDispenserFactory(file.Specification, file.NewSource, file.NewDestination),
 	sdkDispenserFactory(s3.Specification, s3source.NewSource, s3destination.NewDestination),
 }
 
