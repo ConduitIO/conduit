ac7e9600e0713067759e8039c1eb8d83d61b784b
d7t <lott.dylan@gmail.com>
2022-02-28 15:05:30 -0700

    Postres documentation update (#122)
    * Update API URL to use runtime host in production
    
    * Update docker file to node 16
    
    * Update readme with node version
    
    * updates postgres plugin readme to reflect snapshot changes
    
    * updates to documentation and spec
    
    * review changes and spelling
    
    * updates cdc comment in postgres spec
    
    * adds comment reminder to add name property after sdk transition
    
    Co-authored-by: James Martinez <jmar910@gmail.com>
    Co-authored-by: Haris Osmanagić <haris@meroxa.io>

--- pkg/plugins/pg/spec.go
+++ pkg/plugins/pg/spec.go
@@ -21,10 +21,63 @@ type Spec struct{}
 // Specify returns the Plugin's Specification
 func (s Spec) Specify() (plugins.Specification, error) {
 	return plugins.Specification{
-		Summary:           "A PostgreSQL source and destination plugin for Conduit, written in Go.",
-		Version:           "v0.0.1",
-		Author:            "Meroxa, Inc.",
-		DestinationParams: map[string]plugins.Parameter{},
-		SourceParams:      map[string]plugins.Parameter{},
+		// Name: "postgres",// TODO uncomment after plugin is updated to SDK
+		Summary: "A PostgreSQL source and destination plugin for Conduit, written in Go.",
+		Version: "v0.0.1",
+		Author:  "Meroxa, Inc.",
+		DestinationParams: map[string]plugins.Parameter{
+			"url": {
+				Default:     "true",
+				Required:    true,
+				Description: "connection url to the postgres destination.",
+			},
+		},
+		SourceParams: map[string]plugins.Parameter{
+			"table": {
+				Default:     "",
+				Required:    true,
+				Description: "Table name for source to read",
+			},
+			"columns": {
+				Default:     "all columns from table",
+				Required:    false,
+				Description: "Comma-separated list of column names that the iterator should include in payloads.",
+			},
+			"key": {
+				Default:     "primary key of column",
+				Required:    false,
+				Description: "If no key is specified, the connector will attempt to lookup the tables primary key column. if no primary key column is found, then the source will return an error.",
+			},
+			"snapshot": {
+				Default:     "true",
+				Required:    false,
+				Description: "The connector acquires a read-only lock and takes a snapshot of the table before switching into CDC mode.",
+			},
+			"cdc": {
+				Default:     "true",
+				Required:    false,
+				Description: "The connector listens for changes in the specified table. Requires logical replication to be enabled.",
+			},
+			"publication_name": {
+				Default:     "pglogrepl",
+				Required:    false,
+				Description: "Required in CDC mode. Determines which publication the CDC iterator consumes.",
+			},
+			"slot_name": {
+				Default:     "pglogrepl_demo",
+				Required:    false,
+				Description: "Required in CDC mode. Determines which replication slot the CDC iterator uses.",
+			},
+			"url": {
+				Default:     "",
+				Required:    true,
+				Description: "Connection url to the postgres source.",
+			},
+			"replication_url": {
+				Default:     "",
+				Required:    false,
+				Description: "Optional url for the CDC iterator to use instead if a different connection url is required for logical replication.",
+			},
+		},
 	}, nil
 }
