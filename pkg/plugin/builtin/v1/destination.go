60828065955744137893cca5618d5be445761d86
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-04-12 13:10:30 +0200

    Catch panic in built-in connectors (#366)
    * add runSandbox function
    
    * modify runSandbox to use generics and pipe through parameters
    
    * run builtin plugin calls in sandbox mode
    
    * remove todo comment
    
    * remove todo comment

--- pkg/plugin/builtin/v1/destination.go
+++ pkg/plugin/builtin/v1/destination.go
@@ -34,7 +34,6 @@ import (
 // conduit-connector-protocol (cpluginv1). This adapter needs to make sure it behaves in the
 // same way as the standalone plugin adapter, which communicates with the plugin
 // through gRPC, so that the caller can use both of them interchangeably.
-// TODO make sure a panic in a plugin doesn't crash Conduit
 type destinationPluginAdapter struct {
 	impl cpluginv1.DestinationPlugin
 	// logger is used as the internal logger of destinationPluginAdapter.
@@ -64,7 +63,7 @@ func (s *destinationPluginAdapter) Configure(ctx context.Context, cfg map[string
 		return err
 	}
 	s.logger.Trace(ctx).Msg("calling Configure")
-	resp, err := s.impl.Configure(s.withLogger(ctx), req)
+	resp, err := runSandbox(s.impl.Configure, s.withLogger(ctx), req)
 	if err != nil {
 		// TODO create new errors as strings, this happens when they are
 		//  transmitted through gRPC and we don't want to falsely rely on types
@@ -82,7 +81,7 @@ func (s *destinationPluginAdapter) Start(ctx context.Context) error {
 
 	req := toplugin.DestinationStartRequest()
 	s.logger.Trace(ctx).Msg("calling Start")
-	resp, err := s.impl.Start(s.withLogger(ctx), req)
+	resp, err := runSandbox(s.impl.Start, s.withLogger(ctx), req)
 	if err != nil {
 		return err
 	}
@@ -91,7 +90,7 @@ func (s *destinationPluginAdapter) Start(ctx context.Context) error {
 	s.stream = newDestinationRunStream(ctx)
 	go func() {
 		s.logger.Trace(ctx).Msg("calling Run")
-		err := s.impl.Run(s.withLogger(ctx), s.stream)
+		err := runSandboxNoResp(s.impl.Run, s.withLogger(ctx), cpluginv1.DestinationRunStream(s.stream))
 		if err != nil {
 			s.stream.stopAll(cerrors.Errorf("error in run: %w", err))
 		} else {
@@ -149,7 +148,7 @@ func (s *destinationPluginAdapter) Stop(ctx context.Context) error {
 	s.stream.stopSend()
 
 	s.logger.Trace(ctx).Msg("calling Stop")
-	resp, err := s.impl.Stop(s.withLogger(ctx), toplugin.DestinationStopRequest())
+	resp, err := runSandbox(s.impl.Stop, s.withLogger(ctx), toplugin.DestinationStopRequest())
 	if err != nil {
 		return err
 	}
@@ -160,7 +159,7 @@ func (s *destinationPluginAdapter) Stop(ctx context.Context) error {
 
 func (s *destinationPluginAdapter) Teardown(ctx context.Context) error {
 	s.logger.Trace(ctx).Msg("calling Teardown")
-	resp, err := s.impl.Teardown(s.withLogger(ctx), toplugin.DestinationTeardownRequest())
+	resp, err := runSandbox(s.impl.Teardown, s.withLogger(ctx), toplugin.DestinationTeardownRequest())
 	if err != nil {
 		return err
 	}
