d0b6f75009b866b31ced563c73d164068951fe6e
Maha Hajja <maha.d.hajja@gmail.com>
2022-06-24 11:48:48 +0100

    address reviews what's left: * version field * assert first test

--- pkg/conduit/pipelines_parser.go
+++ pkg/conduit/pipelines_parser.go
@@ -15,10 +15,29 @@
 package conduit
 
 import (
+	"io"
+
 	"github.com/conduitio/conduit/pkg/foundation/cerrors"
 	"gopkg.in/yaml.v3"
-	"io"
 )
+
+type ProcessorConfig map[string]struct {
+	Name   string `yaml:"name"`
+	Type   string `yaml:"type"`
+	Config struct {
+		Settings map[string]string `yaml:"settings"`
+	} `yaml:"config"`
+}
+
+type ConnectorConfig map[string]struct {
+	Type   string `yaml:"type"`
+	Plugin string `yaml:"plugin"`
+	Config struct {
+		Name     string            `yaml:"name"`
+		Settings map[string]string `yaml:"settings"`
+	} `yaml:"config"`
+	Processors ProcessorConfig `yaml:"processors,omitempty"`
+}
 
 type PipelineConfig struct {
 	Status string `yaml:"status"`
@@ -26,31 +45,12 @@ type PipelineConfig struct {
 		Name        string `yaml:"name"`
 		Description string `yaml:"description"`
 	} `yaml:"config"`
-	Connectors map[string]struct {
-		Type   string `yaml:"type"`
-		Plugin string `yaml:"plugin"`
-		Config struct {
-			Name     string            `yaml:"name"`
-			Settings map[string]string `yaml:"settings"`
-		} `yaml:"config"`
-		Processors map[string]struct {
-			Name   string `yaml:"name"`
-			Type   string `yaml:"type"`
-			Config struct {
-				Settings map[string]string `yaml:"settings"`
-			} `yaml:"config"`
-		} `yaml:"processors,omitempty"`
-	} `yaml:"connectors,omitempty"`
-	Processors map[string]struct {
-		Name   string `yaml:"name"`
-		Type   string `yaml:"type"`
-		Config struct {
-			Settings map[string]string `yaml:"settings"`
-		} `yaml:"config"`
-	} `yaml:"processors,omitempty"`
+	Connectors ConnectorConfig `yaml:"connectors,omitempty"`
+	Processors ProcessorConfig `yaml:"processors,omitempty"`
 }
 
 type PipelinesConfig struct {
+	Version   string                    `yaml:"version"`
 	Pipelines map[string]PipelineConfig `yaml:"pipelines"`
 }
 
@@ -64,24 +64,30 @@ func Parse(data io.Reader) (PipelinesConfig, error) {
 		if err != nil && cerrors.Is(err, io.EOF) {
 			break
 		} else if err != nil {
-			return PipelinesConfig{}, cerrors.Errorf("error: %w", err)
+			return PipelinesConfig{}, cerrors.Errorf("parsing error: %w", err)
 		}
 		docs = append(docs, doc)
 	}
-	merged := mergeMaps(docs)
+	merged, err := mergeMaps(docs)
+	if err != nil {
+		return PipelinesConfig{}, err
+	}
 	return merged, nil
 }
 
 // mergeMaps takes an array of PipelinesConfig and merges them into one map
-func mergeMaps(arr []PipelinesConfig) PipelinesConfig {
+func mergeMaps(arr []PipelinesConfig) (PipelinesConfig, error) {
 	var mp PipelinesConfig
 	pipelines := make(map[string]PipelineConfig, 0)
 
 	for _, config := range arr {
 		for k, v := range config.Pipelines {
+			if _, ok := pipelines[k]; ok {
+				return PipelinesConfig{}, cerrors.Errorf("found a duplicated pipeline id: %s", k)
+			}
 			pipelines[k] = v
 		}
 	}
 	mp.Pipelines = pipelines
-	return mp
+	return mp, nil
 }
--- pkg/conduit/pipelines_parser_test.go
+++ pkg/conduit/pipelines_parser_test.go
@@ -20,13 +20,26 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"testing"
+
+	"github.com/conduitio/conduit/pkg/foundation/assert"
 )
 
 func Test_Parser(t *testing.T) {
-	filename, err := filepath.Abs("./pipelines.yml")
+	filename, err := filepath.Abs("./test/pipelines1.yml")
 	if err != nil {
 		t.Error(err)
 	}
+
+	//want := &PipelinesConfig{
+	//	Pipelines: map[string]PipelineConfig{
+	//		"pipeline1": {
+	//			Status: "running",
+	//			Config: {"pipeline1", "desc1"},
+	//			Processors: ProcessorConfig{},
+	//		},
+	//		"p": {Status: "stopped"},
+	//	},
+	//}
 
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -38,4 +51,20 @@ func Test_Parser(t *testing.T) {
 		t.Error(err)
 	}
 	fmt.Printf("--- t:\n%v\n\n", p)
+}
+
+func Test_Parser_Duplicate_Pipeline_Id(t *testing.T) {
+	filename, err := filepath.Abs("./test/pipelines2.yml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	p, err := Parse(bytes.NewReader(data))
+	assert.Error(t, err)
+	assert.Equal(t, p, PipelinesConfig{})
 }
