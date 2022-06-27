d6fff0d9d3ea0633e1c25e728f090d534c13b82d
Haris Osmanagić <haris@meroxa.io>
2022-06-27 19:20:52 +0200

    Update pkg/processor/instance.go
    Co-authored-by: Lovro Mažgon <lovro.mazgon@gmail.com>

--- pkg/processor/instance.go
+++ pkg/processor/instance.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate mockgen -destination=mock/processor.go -package=mock -mock_names=Processor=Processor . Processor
+//go:generate mockgen -destination=mock/processor.go -package=mock -mock_names=Interface=Interface . Interface
 //go:generate stringer -type=ParentType -trimprefix ParentType
 
 package processor
