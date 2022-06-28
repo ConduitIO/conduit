94b4f437c20701932a4d6f89779326bbafaca418
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-06-28 20:46:15 +0200

    don't forward acks after a failed ack/nack

--- pkg/pipeline/stream/source_acker.go
+++ pkg/pipeline/stream/source_acker.go
@@ -35,6 +35,10 @@ type SourceAckerNode struct {
 	// sem ensures acks are sent to the source in the correct order and only one
 	// at a time
 	sem semaphore.Simple
+	// fail is set to true once the first ack/nack fails and we can't guarantee
+	// that acks will be delivered in the correct order to the source anymore,
+	// at that point we completely stop processing acks/nacks
+	fail bool
 }
 
 func (n *SourceAckerNode) ID() string {
@@ -71,19 +75,30 @@ func (n *SourceAckerNode) registerAckHandler(msg *Message, ticket semaphore.Tick
 	msg.RegisterAckHandler(
 		func(msg *Message) (err error) {
 			defer func() {
-				tmpErr := n.sem.Release(ticket)
 				if err != nil {
-					// we are already returning an error, log this one instead
-					n.logger.Err(msg.Ctx, tmpErr).Msg("error releasing semaphore ticket for ack")
-					return
+					n.fail = true
 				}
-				err = tmpErr
+				tmpErr := n.sem.Release(ticket)
+				if tmpErr != nil {
+					if err != nil {
+						// we are already returning an error, log this one instead
+						n.logger.Err(msg.Ctx, tmpErr).Msg("error releasing semaphore ticket for ack")
+					} else {
+						err = tmpErr
+					}
+				}
 			}()
 			n.logger.Trace(msg.Ctx).Msg("acquiring semaphore for ack")
 			err = n.sem.Acquire(ticket)
 			if err != nil {
 				return cerrors.Errorf("could not acquire semaphore for ack: %w", err)
 			}
+
+			if n.fail {
+				n.logger.Trace(msg.Ctx).Msg("blocking forwarding of ack to source connector, because another message failed to be acked/nacked")
+				return cerrors.Errorf("another message failed to be acked/nacked")
+			}
+
 			n.logger.Trace(msg.Ctx).Msg("forwarding ack to source connector")
 			return n.Source.Ack(msg.Ctx, msg.Record.Position)
 		},
@@ -94,22 +109,34 @@ func (n *SourceAckerNode) registerNackHandler(msg *Message, ticket semaphore.Tic
 	msg.RegisterNackHandler(
 		func(msg *Message, reason error) (err error) {
 			defer func() {
-				tmpErr := n.sem.Release(ticket)
 				if err != nil {
-					// we are already returning an error, log this one instead
-					n.logger.Err(msg.Ctx, tmpErr).Msg("error releasing semaphore ticket for nack")
-					return
+					n.fail = true
 				}
-				err = tmpErr
+				tmpErr := n.sem.Release(ticket)
+				if tmpErr != nil {
+					if err != nil {
+						// we are already returning an error, log this one instead
+						n.logger.Err(msg.Ctx, tmpErr).Msg("error releasing semaphore ticket for nack")
+					} else {
+						err = tmpErr
+					}
+				}
 			}()
 			n.logger.Trace(msg.Ctx).Msg("acquiring semaphore for nack")
 			err = n.sem.Acquire(ticket)
 			if err != nil {
 				return cerrors.Errorf("could not acquire semaphore for nack: %w", err)
 			}
+
+			if n.fail {
+				n.logger.Trace(msg.Ctx).Msg("blocking forwarding of nack to DLQ handler, because another message failed to be acked/nacked")
+				return cerrors.Errorf("another message failed to be acked/nacked")
+			}
+
 			n.logger.Trace(msg.Ctx).Msg("forwarding nack to DLQ handler")
 			// TODO implement DLQ and call it here, right now any nacked message
-			//  will just stop the pipeline because we don't support DLQs
+			//  will just stop the pipeline because we don't support DLQs,
+			//  don't forget to forward ack to source if the DLQ call succeeds
 			//  https://github.com/ConduitIO/conduit/issues/306
 			return cerrors.New("no DLQ handler configured")
 		},
