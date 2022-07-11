40562d16d22c029a4b85782bc1b643d1fe0f3555
Haris Osmanagic <haris@meroxa.io>
2022-07-11 17:21:11 +0200

    refactor, use makefile

--- test/perf/small-messages-low-rate.sh
+++ test/perf/small-messages-low-rate.sh
@@ -25,8 +25,8 @@ jq -n --arg pipeline_id "$PIPELINE_ID" '{
         {
             "fields": "id:int,name:string,company:string,trial:bool",
             "format": "structured",
-            "readTime": "0ms",
-            "recordCount": "100000"
+            "readTime": "1s",
+            "recordCount": "100"
         }
     }
 }'
