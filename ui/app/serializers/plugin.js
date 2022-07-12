383ac7ba7e4aee036c88616a5b5cc8d6e795a57a
James Martinez <jmar910@gmail.com>
2022-07-12 16:23:47 -0400

    Fix and update supporting dots in key names
    Fixes our normalization/serialization to handle plugin key names with periods.

--- ui/app/serializers/plugin.js
+++ ui/app/serializers/plugin.js
@@ -2,4 +2,15 @@ import ApplicationSerializer from './application';
 
 export default class ConnectorPluginSerializer extends ApplicationSerializer {
   primaryKey = 'name';
+
+  normalize(typeClass, hash) {
+    hash.sourceParams = super._replaceKeys(hash.sourceParams, '.', '@@');
+    hash.destinationParams = super._replaceKeys(
+      hash.destinationParams,
+      '.',
+      '@@'
+    );
+
+    return super.normalize(typeClass, hash);
+  }
 }
