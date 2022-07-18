42ec60f440c09aa3f8ecd69940df60fdbfcab991
Haris Osmanagic <haris@meroxa.io>
2022-07-18 14:55:31 +0200

    perf benchmark demo

---
+++ test/perf/small-messages-burst.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+echo "Creating pipeline..."
+PIPELINE_ID=$(
+curl -Ss -X POST 'http://localhost:8080/v1/pipelines' -d '
+{
+    "config":
+    {
+        "name": "perf-test",
+        "description": "Test performance"
+    }
+}' | jq -r '.id'
+)
+
+
+echo "Creating a generator source..."
+SOURCE_CONN_REQ_1=$(
+jq -n --arg pipeline_id "$PIPELINE_ID" '{
+    "type": "TYPE_SOURCE",
+    "plugin": "builtin:generator",
+    "pipeline_id": $pipeline_id,
+    "config":
+    {
+        "name": "generator-source-1",
+        "settings":
+        {
+            "format.options": "id:int,name:string,company:string,trial:bool",
+            "format.type": "structured",
+            "readTime": "1s",
+            "recordCount": "-1"
+        }
+    }
+}'
+)
+CONNECTOR_ID_1=$(curl -Ss -X POST 'http://localhost:8080/v1/connectors' -d "$SOURCE_CONN_REQ_1" | jq -r '.id')
+
+echo "Creating a generator source..."
+SOURCE_CONN_REQ_2=$(
+jq -n --arg pipeline_id "$PIPELINE_ID" '{
+    "type": "TYPE_SOURCE",
+    "plugin": "builtin:generator",
+    "pipeline_id": $pipeline_id,
+    "config":
+    {
+        "name": "generator-source-2",
+        "settings":
+        {
+            "format.options": "id:int,name:string,company:string,trial:bool",
+            "format.type": "structured",
+            "sleepTime": "10s",
+            "generateTime": "60s",
+            "readTime": "1ms",
+            "recordCount": "-1"
+        }
+    }
+}'
+)
+CONNECTOR_ID_2=$(curl -Ss -X POST 'http://localhost:8080/v1/connectors' -d "$SOURCE_CONN_REQ_2" | jq -r '.id')
+
+echo "Creating a file destination..."
+DEST_CONN_REQ=$(
+jq -n  --arg pipeline_id "$PIPELINE_ID" '{
+     "type": "TYPE_DESTINATION",
+     "plugin": "builtin:file",
+     "pipeline_id": $pipeline_id,
+     "config":
+     {
+         "name": "my-file-destination",
+         "settings":
+         {
+             "path": "/file_destination.txt"
+         }
+     }
+ }'
+)
+
+curl -Ss -X POST 'http://localhost:8080/v1/connectors' -d "$DEST_CONN_REQ" > /dev/null
+
+echo "Starting the pipeline..."
+curl -Ss -X POST "http://localhost:8080/v1/pipelines/$PIPELINE_ID/start" > /dev/null
+echo "Pipeline started!"
