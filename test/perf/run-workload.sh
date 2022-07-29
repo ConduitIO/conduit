193b31872dca36645510cf370629847c7537f385
Haris Osmanagic <haris@meroxa.io>
2022-07-29 12:36:22 +0200

    change default duration of test

--- test/perf/run-workload.sh
+++ test/perf/run-workload.sh
@@ -13,4 +13,4 @@ sleep 1
 bash "$WORKLOAD"
 
 # todo print all results from this run into the same file
-go run main.go --interval=30s --duration=30s --print-to=csv --workload="$WORKLOAD"
+go run main.go --interval=5m --duration=5m --print-to=csv --workload="$WORKLOAD"
