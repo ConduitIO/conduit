85922943b0ff6f8a2979c83d32c5f2a99a2eda55
Haris Osmanagic <haris@meroxa.io>
2022-07-29 15:34:05 +0200

    PR feeback

--- test/perf/run-workload.sh
+++ test/perf/run-workload.sh
@@ -13,6 +13,6 @@ sleep 1
 bash "$WORKLOAD"
 
 # todo print all results from this run into the same file
-go run main.go --interval=5s --duration=2m --print-to=console --workload="$WORKLOAD"
+go run main.go --interval=5m --duration=5m --print-to=csv --workload="$WORKLOAD"
 
 docker stop conduit-perf-test || true
--- test/perf/run.sh
+++ test/perf/run.sh
@@ -2,6 +2,5 @@
 CONDUIT_IMAGE=$1
 
 for w in workloads/*.sh; do
-
   ./run-workload.sh $CONDUIT_IMAGE "$w"
 done
