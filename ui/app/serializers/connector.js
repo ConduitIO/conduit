629fac1a10339b4f94b762348ffb6928b2cc973f
James Martinez <jmar910@gmail.com>
2022-09-27 11:34:26 -0400

    Refactor plugin lookup to its own function (#653)

--- ui/app/serializers/connector.js
+++ ui/app/serializers/connector.js
@@ -25,90 +25,93 @@ export default class ConnectorSerializer extends ApplicationSerializer {
     };
   }
 
+  _pluginLookup(pluginNames, matchTo) {
+    const hasOnlyType = /^(any|builtin|standalone):[\w-]+$/.test(matchTo);
+    const hasOnlyVersion = /^[\w-]+@v\S+$/.test(matchTo);
+    const hasTypeAndVersion = /^(any|builtin|standalone):[\w-]+@v\S+$/.test(
+      matchTo
+    );
+
+    let pluginName = matchTo;
+    let pluginType = 'any';
+    let pluginVersion = 'latest';
+
+    if (hasOnlyType) {
+      pluginType = matchTo.split(':')[0];
+      pluginName = matchTo.split(':')[1];
+    }
+
+    if (hasOnlyVersion) {
+      pluginName = matchTo.split('@v')[0];
+      pluginVersion = matchTo.split('@v')[1];
+    }
+
+    if (hasTypeAndVersion) {
+      const split = matchTo
+        .replaceAll(':', '@@')
+        .replaceAll('@v', '@@')
+        .split('@@');
+
+      pluginType = split[0];
+      pluginName = split[1];
+      pluginVersion = split[2];
+    }
+
+    const filtered = pluginNames.filter((name) => {
+      return name.indexOf(pluginName) !== -1;
+    });
+
+    if (filtered.length === 1) {
+      return {
+        id: filtered[0],
+        type: 'plugin',
+      };
+    }
+
+    const latestVersion = filtered.reduce((acc, name) => {
+      const accVersion = acc.split('@v')[1];
+      const version = name.split('@v')[1];
+      if (accVersion > version) {
+        return accVersion;
+      } else {
+        return version;
+      }
+    }, '');
+
+    let pluginMatch;
+
+    if (pluginType === 'any') {
+      pluginType = 'standalone';
+    }
+
+    if (pluginVersion === 'latest') {
+      pluginVersion = latestVersion;
+    }
+
+    pluginMatch =
+      filtered.find((name) => {
+        return (
+          name.indexOf(`${pluginType}:${pluginName}@v${pluginVersion}`) !== -1
+        );
+      }) ||
+      filtered.find((name) => {
+        return name.indexOf(`builtin:${pluginName}@v${pluginVersion}`) !== -1;
+      });
+
+    if (!pluginMatch) {
+      return super.extractRelationship('plugin', matchTo);
+    }
+
+    return {
+      id: pluginMatch,
+      type: 'plugin',
+    };
+  }
+
   extractRelationship(modelName, value) {
     if (modelName === 'plugin' && value) {
       const pluginNames = this.store.peekAll('plugin').mapBy('name');
-
-      const hasOnlyType = /^(any|builtin|standalone):[\w-]+$/.test(value);
-      const hasOnlyVersion = /^[\w-]+@v\S+$/.test(value);
-      const hasTypeAndVersion = /(any|builtin|standalone):[\w-]+@v\S+$/.test(
-        value
-      );
-
-      let pluginName = value;
-      let pluginType = 'any';
-      let pluginVersion = 'latest';
-
-      if (hasOnlyType) {
-        pluginType = value.split(':')[0];
-        pluginName = value.split(':')[1];
-      }
-
-      if (hasOnlyVersion) {
-        pluginName = value.split('@')[0];
-        pluginVersion = value.split('@')[1];
-      }
-
-      if (hasTypeAndVersion) {
-        const split = value
-          .replaceAll(':', '@@')
-          .replaceAll('@v', '@@')
-          .split('@@');
-
-        pluginType = split[0];
-        pluginName = split[1];
-        pluginVersion = split[2];
-      }
-
-      const filtered = pluginNames.filter((name) => {
-        return name.indexOf(pluginName) !== -1;
-      });
-
-      if (filtered.length === 1) {
-        return {
-          id: filtered[0],
-          type: modelName,
-        };
-      }
-
-      const latestVersion = filtered.reduce((acc, name) => {
-        const accVersion = acc.split('@v')[1];
-        const version = name.split('@v')[1];
-        if (accVersion > version) {
-          return accVersion;
-        } else {
-          return version;
-        }
-      }, '');
-
-      let pluginMatch;
-
-      if (pluginType === 'any') {
-        pluginType = 'standalone';
-      }
-
-      if (pluginVersion === 'latest') {
-        pluginVersion = latestVersion;
-      }
-
-      pluginMatch =
-        filtered.find((name) => {
-          return (
-            name.indexOf(`${pluginType}:${pluginName}@${latestVersion}`) !== -1
-          );
-        }) ||
-        filtered.find((name) => {
-          return name.indexOf(`builtin:${pluginName}@${latestVersion}`) !== -1;
-        });
-
-      if (!pluginMatch) {
-        return super.extractRelationship(modelName, value);
-      }
-
-      return {
-        id: pluginMatch,
-        type: modelName,
-      };
+      return this._pluginLookup(pluginNames, value);
     }
 
     return super.extractRelationship(modelName, value);
