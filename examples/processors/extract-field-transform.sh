26d49c3bfd211406d9337eba39838eb7b58d0548
Haris Osmanagic <haris@meroxa.io>
2022-10-20 12:08:19 +0200

    update docs

--- examples/processors/extract-field-transform.sh
+++ examples/processors/extract-field-transform.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-# This script will start Conduit and set up a pipeline which demonstrates
-# a transform replacing a record's structured payload with one of its fields.
-# The pipeline is define in pipeline-extract-field-transform.yml.
-# The generator source will generate 3 records, which are in the format of this example:
+# In this example, we have a processor which replaces a record's
+# structured payload with one of its fields.
 
+# The whole pipeline is define in pipeline-extract-field-transform.yml.
+# It has a generator source which will generates records.
+# The records are in the format of this example:
+#{
+#  "company": "string 5c778521-ca78-4aab-8ddd-03e541f38fcc",
+#  "id": 384814097,
+#  "name": "string 85227f17-120e-447c-9e46-c7d57c6dc8ec",
+#  "trial": false
+#}
+# The records are replaced with the name of the "name" field, using a built-in processor,
+# and the output is then written to a destination file.
+
+# Prepare the destination file
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DEST_FILE="$SCRIPT_DIR/file_destination.txt"
 > "$DEST_FILE"
@@ -20,6 +31,9 @@ docker run --rm  \
 echo "Pipeline started! Let's give the pipeline some time to run."
 sleep 1
 
+# Conduit records come in the OpenCDC format
+# (more information is available at: https://github.com/ConduitIO/conduit/blob/main/docs/design-documents/20220309-opencdc.md).
+# We use jq to extract just the payload.
 echo "Let's check the destination file contents:"
 cat "$DEST_FILE" | jq -r '.payload.after | @base64d'
 
