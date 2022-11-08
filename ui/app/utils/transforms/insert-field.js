16871f4ce238285620639c31e8ac2ffd642d5cb2
DeVaris Brown <devaris@meroxa.io>
2022-11-07 20:34:12 -0800

    Update descriptions for hoist and insert transforms

--- ui/app/utils/transforms/insert-field.js
+++ ui/app/utils/transforms/insert-field.js
@@ -3,7 +3,7 @@ import Transform from './transform';
 export default class InsertField extends Transform {
   static id = 'insertfield';
   static label = 'InsertField';
-  static description = '';
+  static description = 'Insert fields using attributes from the record metadata or a configured static value.';
 
   static blueprint = {
     'static.field': {
