48081a24395b0d5861feef30650ca2587ed95138
Haris Osmanagic <haris@meroxa.io>
2022-11-10 13:29:45 +0100

    copy swagger json

---
+++ pkg/web/openapi/copy-swagger-api.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# This script is used to copy the swagger API that is generated remotely by buf
+# into a local folder used by the swagger-ui.
+cp ../../../proto/gen/api/v1/api.swagger.json ./swagger-ui/api/v1/api.swagger.json
