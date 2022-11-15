885e3eefe08b99fb2a29096363578b8f8b96cbc8
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-11-15 15:10:36 +0100

    optimize csync.Init

--- pkg/foundation/csync/init.go
+++ pkg/foundation/csync/init.go
@@ -17,42 +17,42 @@ package csync
 import (
 	"context"
 	"sync"
-
-	"github.com/conduitio/conduit/pkg/foundation/cchan"
 )
 
 // Init waits for a main goroutine to finish initialization before releasing
 // other goroutines.
+// Init can be compared to a WaitGroup which starts with the counter set to 1
+// and ignores calls to Done after the counter reaches 0.
 type Init struct {
 	initOnce sync.Once
-	done     chan struct{}
+	doneOnce sync.Once
+	waitOnce sync.Once
+	wg       WaitGroup
 }
 
 func (i *Init) init() {
 	i.initOnce.Do(func() {
-		i.done = make(chan struct{})
+		i.wg.Add(1)
 	})
 }
 
 // Done should be called by the goroutine responsible for initializing. Once
 // this call finishes any other goroutines calling Wait will be released. Done
 // can be called multiple times, only the first call will have an effect.
-// This function is not safe for concurrent use.
 func (i *Init) Done() {
-	i.init()
-	select {
-	case <-i.done:
-		// i.done is closed, all good
-	default:
-		// close i.done if it's still open
-		close(i.done)
-	}
+	i.doneOnce.Do(func() {
+		i.init()
+		i.wg.Done()
+	})
 }
 
 // Wait can be called by a gorotuine to wait for another goroutine to finish
 // initializing the object. This function is safe for concurrent use.
 func (i *Init) Wait(ctx context.Context) error {
-	i.init()
-	_, _, err := cchan.Chan[struct{}](i.done).Recv(ctx)
-	return err
+	i.waitOnce.Do(func() {
+		i.init()
+		// ignore the error, we will return the context error anyway
+		_ = i.wg.Wait(ctx)
+	})
+	return ctx.Err()
 }
