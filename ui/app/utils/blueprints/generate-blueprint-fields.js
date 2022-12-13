64f2f140d6cbccdbaa442802e778da9a78862ebd
James Martinez <jmar910@gmail.com>
2022-12-13 07:47:19 -0500

    fix(ui): optional fields allow blank

--- ui/app/utils/blueprints/generate-blueprint-fields.js
+++ ui/app/utils/blueprints/generate-blueprint-fields.js
@@ -14,36 +14,39 @@ const ConfigValidationMap = {
     return validatePresence(true);
   },
 
-  TYPE_GREATER_THAN: function (value, type) {
-    const parseFn = type === 'TYPE_INT' ? parseInt : parseFloat;
-    const options = { gt: parseFn(value) };
+  TYPE_GREATER_THAN: function (value, fieldType, isRequired) {
+    const parseFn = fieldType === 'TYPE_INT' ? parseInt : parseFloat;
+    const options = { gt: parseFn(value), allowBlank: !isRequired };
     return validateNumber(options);
   },
 
-  TYPE_LESS_THAN: function (value, type) {
-    const parseFn = type === 'TYPE_INT' ? parseInt : parseFloat;
-    const options = { lt: parseFn(value) };
+  TYPE_LESS_THAN: function (value, fieldType, isRequired) {
+    const parseFn = fieldType === 'TYPE_INT' ? parseInt : parseFloat;
+    const options = { lt: parseFn(value), allowBlank: !isRequired };
     return validateNumber(options);
   },
 
-  TYPE_INCLUSION: function (value = '') {
-    const options = { list: value.split(',') };
+  TYPE_INCLUSION: function (value = '', fieldType, isRequired) {
+    const options = { list: value.split(','), allowBlank: !isRequired };
     return validateInclusion(options);
   },
 
-  TYPE_EXCLUSION: function (value = '') {
+  TYPE_EXCLUSION: function (value = '', fieldType, isRequired) {
     const options = {
       list: value.split(','),
       message: '{description} cannot be any of ({list})',
+      allowBlank: !isRequired,
     };
     return validateExclusion(options);
   },
 
-  TYPE_REGEX: function (value) {
+  TYPE_REGEX: function (value, fieldType, isRequired) {
     const options = {
       regex: new RegExp(value),
       message: '{description} must match regex {regex}',
+      allowBlank: !isRequired,
     };
+
     return validateFormat(options);
   },
 };
@@ -55,10 +58,12 @@ export default function generateBlueprintFields(blueprint, configurable) {
     const fieldOpts = blueprint[fieldName];
     const currentConfig = configurable.get(`config.settings.${fieldName}`);
     const currentConfigValue = currentConfig ? currentConfig : null;
+    const isRequired = !!fieldOpts.validations.findBy('type', 'TYPE_REQUIRED');
 
     const validations = generateConfigValidations(
       fieldOpts.validations,
-      fieldOpts.type
+      fieldOpts.type,
+      isRequired
     );
 
     const fieldModel = {
@@ -66,7 +71,7 @@ export default function generateBlueprintFields(blueprint, configurable) {
       label: underscore(fieldName).replace('@@', '_').split('_').join(' '),
       description: fieldOpts.description,
       type: fieldOpts.type,
-      isRequired: !!fieldOpts.validations.findBy('type', 'TYPE_REQUIRED'),
+      isRequired,
       rawValidations: fieldOpts.validations,
       value: currentConfigValue,
       hasUserTakenAction: false,
@@ -76,9 +81,13 @@ export default function generateBlueprintFields(blueprint, configurable) {
   });
 }
 
-function generateConfigValidations(fieldValidations) {
+function generateConfigValidations(fieldValidations, fieldType, isRequired) {
   const validations = fieldValidations.map((validation) => {
-    return ConfigValidationMap[validation.type](validation.value);
+    return ConfigValidationMap[validation.type](
+      validation.value,
+      fieldType,
+      isRequired
+    );
   });
 
   return {
