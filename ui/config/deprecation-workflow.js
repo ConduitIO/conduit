f93fead5abeaf84d5c0412bc9b698efb66d0d58d
James Martinez <jmar910@gmail.com>
2023-01-09 17:03:19 -0500

    Remove deprecation workflow entries

--- ui/config/deprecation-workflow.js
+++ ui/config/deprecation-workflow.js
@@ -2,18 +2,5 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 
 self.deprecationWorkflow.config = {
-  workflow: [
-    { handler: 'silence', matchId: 'ember-keyboard.first-responder-inputs' },
-    { handler: 'silence', matchId: 'ember-test-waiters-legacy-module-name' },
-    { handler: 'silence', matchId: 'ember-keyboard.old-propagation-model' },
-    {
-      handler: 'silence',
-      matchId: 'ember.built-in-components.legacy-arguments',
-    },
-
-    {
-      handler: 'silence',
-      matchId: 'ember-modifier.function-based-options',
-    },
-  ],
+  workflow: [],
 };
