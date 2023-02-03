a4ffc46e47ba9614112679435a52da332d235a19
Lovro Mažgon <lovro.mazgon@gmail.com>
2023-02-03 15:44:29 +0100

    get go version from go list

--- scripts/check-go-version.sh
+++ scripts/check-go-version.sh
@@ -6,9 +6,8 @@
 function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
 
 GO_VERSION=$(go version | { read -r _ _ v _; echo "${v#go}"; })
-# needs to be changed each time conduit Go version is upgraded
-REQUIRED_GO_VERSION="1.20"
+MIN_GO_VERSION=$(go list -m -f "{{.GoVersion}}")
 
-if version_lt "$GO_VERSION" "$REQUIRED_GO_VERSION"; then
-    echo "ERROR: min Go version required is go$REQUIRED_GO_VERSION, version installed is go$GO_VERSION"
+if version_lt "$GO_VERSION" "$MIN_GO_VERSION"; then
+    echo "ERROR: min Go version required is go$MIN_GO_VERSION, version installed is go$GO_VERSION"
 fi
