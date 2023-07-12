25faea6f8db2286af038506e806cc67324a7ea92
Lovro Mažgon <lovro.mazgon@gmail.com>
2023-07-12 13:25:17 +0200

    extract switch cases into methods

--- pkg/processor/schemaregistry/avro/extractor.go
+++ pkg/processor/schemaregistry/avro/extractor.go
@@ -54,161 +54,116 @@ func (e extractor) extractInternal(path []string, v reflect.Value, t reflect.Typ
 	case reflect.String:
 		return avro.NewPrimitiveSchema(avro.String, nil), nil
 	case reflect.Pointer:
-		var vElem reflect.Value
-		if v.IsValid() {
-			vElem = v.Elem()
-		}
-		s, err := e.extractInternal(path, vElem, t.Elem())
-		if err != nil {
-			return nil, err
-		}
-
-		var schemas avro.Schemas
-		if us, ok := s.(*avro.UnionSchema); ok && us.Nullable() {
-			// it's already a nullable schema
-			return s, nil
-		} else if ok {
-			// take types from union schema
-			schemas = us.Types()
-		} else if s.Type() != avro.Null {
-			// non-nil type
-			schemas = avro.Schemas{s}
-		}
-
-		s, err = avro.NewUnionSchema(append(schemas, &avro.NullSchema{}))
-		if err != nil {
-			return nil, err
-		}
-
-		return s, nil
+		return e.extractPointer(path, v, t)
 	case reflect.Interface:
-		if !v.IsValid() || v.IsNil() {
-			// unknown type, fall back to nullable string
-			return avro.NewUnionSchema([]avro.Schema{
-				avro.NewPrimitiveSchema(avro.String, nil),
-				&avro.NullSchema{},
-			})
-		}
-		return e.extractInternal(path, v.Elem(), v.Elem().Type())
+		return e.extractInterface(path, v, t)
 	case reflect.Array:
 		if t.Elem() != byteType {
 			return nil, cerrors.Errorf("arrays with value type %v not supported, avro only supports bytes as values", t.Elem().String())
 		}
 		return avro.NewFixedSchema(strings.Join(path, "."), "", t.Len(), nil)
 	case reflect.Slice:
-		if t.Elem().Kind() == reflect.Uint8 {
-			return avro.NewPrimitiveSchema(avro.Bytes, nil), nil
-		}
-
-		// try getting value type based on the slice type
-		if t.Elem().Kind() != reflect.Interface {
-			vs, err := e.extractInternal(append(path, "item"), reflect.Value{}, t.Elem())
-			if err != nil {
-				return nil, err
-			}
-			return avro.NewArraySchema(vs), nil
-		}
-
-		// this is []any, loop through all values and extracting their types
-		// into a union schema, null is included by default
-		types := []avro.Schema{&avro.NullSchema{}}
-		for i := 0; i < v.Len(); i++ {
-			itemSchema, err := e.extractInternal(
-				append(path, fmt.Sprintf("item%d", i)),
-				v.Index(i), t.Elem(),
-			)
-			if err != nil {
-				return nil, err
-			}
-			types = e.appendSchemas(types, itemSchema)
-		}
-		if v.Len() == 0 {
-			// it's an empty slice, add string to types to have a valid schema
-			types = append(types, avro.NewPrimitiveSchema(avro.String, nil))
-		}
-
-		itemsSchema, err := avro.NewUnionSchema(types)
-		if err != nil {
-			return nil, cerrors.Errorf("%s: %w", strings.Join(path, "."), err)
-		}
-		return avro.NewArraySchema(itemsSchema), nil
+		return e.extractSlice(path, v, t)
 	case reflect.Map:
-		if t == structuredDataType {
-			// special case - we treat StructuredData like a struct
-			var fields []*avro.Field
-			valType := t.Elem()
-			for _, keyValue := range v.MapKeys() {
-				fs, err := e.extractInternal(append(path, keyValue.String()), v.MapIndex(keyValue), valType)
-				if err != nil {
-					return nil, err
-				}
-				field, err := avro.NewField(keyValue.String(), fs)
-				if err != nil {
-					return nil, err
-				}
-				fields = append(fields, field)
-			}
-			rs, err := avro.NewRecordSchema(strings.Join(path, "."), "", fields)
-			if err != nil {
-				return nil, err
-			}
-			return rs, nil
-		}
-		if t.Key().Kind() != reflect.String {
-			return nil, cerrors.Errorf("%s: maps with key type %v not supported, avro only supports strings as keys", strings.Join(path, "."), t.Key().Kind())
-		}
-		// try getting value type based on the map type
-		if t.Elem().Kind() != reflect.Interface {
-			vs, err := e.extractInternal(append(path, "value"), reflect.Value{}, t.Elem())
-			if err != nil {
-				return nil, err
-			}
-			return avro.NewMapSchema(vs), nil
-		}
-
-		// this is map[string]any, loop through all values and extracting their
-		// types into a union schema, null is included by default
-		types := []avro.Schema{&avro.NullSchema{}}
-		typesSet := make(map[[32]byte]struct{})
-		for _, kv := range v.MapKeys() {
-			valValue := v.MapIndex(kv)
-			vs, err := e.extractInternal(append(path, "value"), valValue, t.Elem())
-			if err != nil {
-				return nil, err
-			}
-			if _, ok := typesSet[vs.Fingerprint()]; ok {
-				continue
-			}
-			typesSet[vs.Fingerprint()] = struct{}{}
-			types = e.appendSchemas(types, vs)
-		}
-		if len(v.MapKeys()) == 0 {
-			// it's an empty map, add string to types to have a valid schema
-			types = append(types, avro.NewPrimitiveSchema(avro.String, nil))
-		}
-		vs, err := avro.NewUnionSchema(types)
-		if err != nil {
-			return nil, cerrors.Errorf("%s: %w", strings.Join(path, "."), err)
-		}
-		return avro.NewMapSchema(vs), nil
+		return e.extractMap(path, v, t)
 	case reflect.Struct:
+		return e.extractStruct(path, v, t)
+	}
+	// Invalid, Uintptr, UnsafePointer, Uint64, Uint, Complex64, Complex128, Chan, Func
+	return nil, fmt.Errorf("unsupported type: %v", t)
+}
+
+func (e extractor) extractPointer(path []string, v reflect.Value, t reflect.Type) (avro.Schema, error) {
+	var vElem reflect.Value
+	if v.IsValid() {
+		vElem = v.Elem()
+	}
+	s, err := e.extractInternal(path, vElem, t.Elem())
+	if err != nil {
+		return nil, err
+	}
+
+	var schemas avro.Schemas
+	if us, ok := s.(*avro.UnionSchema); ok && us.Nullable() {
+		// it's already a nullable schema
+		return s, nil
+	} else if ok {
+		// take types from union schema
+		schemas = us.Types()
+	} else if s.Type() != avro.Null {
+		// non-nil type
+		schemas = avro.Schemas{s}
+	}
+
+	s, err = avro.NewUnionSchema(append(schemas, &avro.NullSchema{}))
+	if err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+
+func (e extractor) extractInterface(path []string, v reflect.Value, _ reflect.Type) (avro.Schema, error) {
+	if !v.IsValid() || v.IsNil() {
+		// unknown type, fall back to nullable string
+		return avro.NewUnionSchema([]avro.Schema{
+			avro.NewPrimitiveSchema(avro.String, nil),
+			&avro.NullSchema{},
+		})
+	}
+	return e.extractInternal(path, v.Elem(), v.Elem().Type())
+}
+
+func (e extractor) extractSlice(path []string, v reflect.Value, t reflect.Type) (avro.Schema, error) {
+	if t.Elem().Kind() == reflect.Uint8 {
+		return avro.NewPrimitiveSchema(avro.Bytes, nil), nil
+	}
+
+	// try getting value type based on the slice type
+	if t.Elem().Kind() != reflect.Interface {
+		vs, err := e.extractInternal(append(path, "item"), reflect.Value{}, t.Elem())
+		if err != nil {
+			return nil, err
+		}
+		return avro.NewArraySchema(vs), nil
+	}
+
+	// this is []any, loop through all values and extracting their types
+	// into a union schema, null is included by default
+	types := []avro.Schema{&avro.NullSchema{}}
+	for i := 0; i < v.Len(); i++ {
+		itemSchema, err := e.extractInternal(
+			append(path, fmt.Sprintf("item%d", i)),
+			v.Index(i), t.Elem(),
+		)
+		if err != nil {
+			return nil, err
+		}
+		types = e.appendSchemas(types, itemSchema)
+	}
+	if v.Len() == 0 {
+		// it's an empty slice, add string to types to have a valid schema
+		types = append(types, avro.NewPrimitiveSchema(avro.String, nil))
+	}
+
+	itemsSchema, err := avro.NewUnionSchema(types)
+	if err != nil {
+		return nil, cerrors.Errorf("%s: %w", strings.Join(path, "."), err)
+	}
+	return avro.NewArraySchema(itemsSchema), nil
+}
+
+func (e extractor) extractMap(path []string, v reflect.Value, t reflect.Type) (avro.Schema, error) {
+	if t == structuredDataType {
+		// special case - we treat StructuredData like a struct
 		var fields []*avro.Field
-		for i := 0; i < t.NumField(); i++ {
-			sf := t.Field(i)
-			name, ok := e.getStructFieldJSONName(sf)
-			if !ok {
-				continue // skip this field
-			}
-			var vfi reflect.Value
-			if v.IsValid() {
-				vfi = v.Field(i)
-			}
-			fs, err := e.extractInternal(append(path, name), vfi, t.Field(i).Type)
+		valType := t.Elem()
+		for _, keyValue := range v.MapKeys() {
+			fs, err := e.extractInternal(append(path, keyValue.String()), v.MapIndex(keyValue), valType)
 			if err != nil {
 				return nil, err
 			}
-
-			field, err := avro.NewField(name, fs)
+			field, err := avro.NewField(keyValue.String(), fs)
 			if err != nil {
 				return nil, err
 			}
@@ -220,8 +175,73 @@ func (e extractor) extractInternal(path []string, v reflect.Value, t reflect.Typ
 		}
 		return rs, nil
 	}
-	// Invalid, Uintptr, UnsafePointer, Uint64, Uint, Complex64, Complex128, Chan, Func
-	return nil, fmt.Errorf("unsupported type: %v", t)
+	if t.Key().Kind() != reflect.String {
+		return nil, cerrors.Errorf("%s: maps with key type %v not supported, avro only supports strings as keys", strings.Join(path, "."), t.Key().Kind())
+	}
+	// try getting value type based on the map type
+	if t.Elem().Kind() != reflect.Interface {
+		vs, err := e.extractInternal(append(path, "value"), reflect.Value{}, t.Elem())
+		if err != nil {
+			return nil, err
+		}
+		return avro.NewMapSchema(vs), nil
+	}
+
+	// this is map[string]any, loop through all values and extracting their
+	// types into a union schema, null is included by default
+	types := []avro.Schema{&avro.NullSchema{}}
+	typesSet := make(map[[32]byte]struct{})
+	for _, kv := range v.MapKeys() {
+		valValue := v.MapIndex(kv)
+		vs, err := e.extractInternal(append(path, "value"), valValue, t.Elem())
+		if err != nil {
+			return nil, err
+		}
+		if _, ok := typesSet[vs.Fingerprint()]; ok {
+			continue
+		}
+		typesSet[vs.Fingerprint()] = struct{}{}
+		types = e.appendSchemas(types, vs)
+	}
+	if len(v.MapKeys()) == 0 {
+		// it's an empty map, add string to types to have a valid schema
+		types = append(types, avro.NewPrimitiveSchema(avro.String, nil))
+	}
+	vs, err := avro.NewUnionSchema(types)
+	if err != nil {
+		return nil, cerrors.Errorf("%s: %w", strings.Join(path, "."), err)
+	}
+	return avro.NewMapSchema(vs), nil
+}
+
+func (e extractor) extractStruct(path []string, v reflect.Value, t reflect.Type) (avro.Schema, error) {
+	var fields []*avro.Field
+	for i := 0; i < t.NumField(); i++ {
+		sf := t.Field(i)
+		name, ok := e.getStructFieldJSONName(sf)
+		if !ok {
+			continue // skip this field
+		}
+		var vfi reflect.Value
+		if v.IsValid() {
+			vfi = v.Field(i)
+		}
+		fs, err := e.extractInternal(append(path, name), vfi, t.Field(i).Type)
+		if err != nil {
+			return nil, err
+		}
+
+		field, err := avro.NewField(name, fs)
+		if err != nil {
+			return nil, err
+		}
+		fields = append(fields, field)
+	}
+	rs, err := avro.NewRecordSchema(strings.Join(path, "."), "", fields)
+	if err != nil {
+		return nil, err
+	}
+	return rs, nil
 }
 
 func (e extractor) appendSchemas(schemas []avro.Schema, additionalSchemas ...avro.Schema) []avro.Schema {
