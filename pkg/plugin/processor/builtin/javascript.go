862773752cbb1dabcaf80c34ffc5fcf210fd09fb
Haris Osmanagic <haris@meroxa.io>
2024-02-08 16:30:03 +0100

    javascript processor tests

--- pkg/plugin/processor/builtin/javascript.go
+++ pkg/plugin/processor/builtin/javascript.go
@@ -16,14 +16,55 @@ package builtin
 
 import (
 	"context"
+	"os"
+	"sync"
+
 	"github.com/conduitio/conduit-commons/opencdc"
 	sdk "github.com/conduitio/conduit-processor-sdk"
+	"github.com/conduitio/conduit/pkg/foundation/cerrors"
+	"github.com/conduitio/conduit/pkg/foundation/log"
+	"github.com/conduitio/conduit/pkg/record"
+	"github.com/dop251/goja"
+	"github.com/dop251/goja_nodejs/require"
 )
 
-type jsProcessor struct {
+const entrypoint = "process"
+
+// jsRecord is an intermediary representation of record.Record that is passed to
+// the JavaScript transform. We use this because using record.Record would not
+// allow us to modify or access certain data (e.g. metadata or structured data).
+type jsRecord struct {
+	Position  []byte
+	Operation string
+	Metadata  map[string]string
+	Key       any
+	Payload   struct {
+		Before any
+		After  any
+	}
 }
 
-func (j jsProcessor) Specification() (sdk.Specification, error) {
+// gojaContext represents one independent goja context.
+type gojaContext struct {
+	runtime  *goja.Runtime
+	function goja.Callable
+}
+
+type jsProcessor struct {
+	sdk.UnimplementedProcessor
+
+	// src is the JavaScript code that will be executed
+	src string
+
+	gojaPool sync.Pool
+	logger   log.CtxLogger
+}
+
+func newJsProcessor(logger log.CtxLogger) *jsProcessor {
+	return &jsProcessor{logger: logger}
+}
+
+func (p *jsProcessor) Specification() (sdk.Specification, error) {
 	return sdk.Specification{
 		Name:        "custom.javascript",
 		Summary:     "",
@@ -47,27 +88,219 @@ func (j jsProcessor) Specification() (sdk.Specification, error) {
 	}, nil
 }
 
-func (j jsProcessor) Configure(ctx context.Context, m map[string]string) error {
-	//TODO implement me
-	panic("implement me")
+func (p *jsProcessor) Configure(_ context.Context, m map[string]string) error {
+	script, hasScript := m["script"]
+	scriptPath, hasScriptPath := m["script.path"]
+	switch {
+	case hasScript && hasScriptPath:
+		return cerrors.New("only one of: [script, script.path] should be provided")
+	case hasScript:
+		p.src = script
+	case hasScriptPath:
+		file, err := os.ReadFile(scriptPath)
+		if err != nil {
+			return cerrors.Errorf("error reading script from path %v: %w", scriptPath, err)
+		}
+		p.src = string(file)
+	}
+
+	return nil
 }
 
-func (j jsProcessor) Open(ctx context.Context) error {
-	//TODO implement me
-	panic("implement me")
+func (p *jsProcessor) Open(context.Context) error {
+	runtime, err := p.newJSRuntime(p.logger)
+	if err != nil {
+		return cerrors.Errorf("failed initializing JS runtime: %w", err)
+	}
+
+	_, err = p.newFunction(runtime, p.src)
+	if err != nil {
+		return cerrors.Errorf("failed initializing JS function: %w", err)
+	}
+
+	p.gojaPool.New = func() any {
+		// create a new runtime for the function, so it's executed in a separate goja context
+		rt, _ := p.newJSRuntime(p.logger)
+		f, _ := p.newFunction(rt, p.src)
+		return &gojaContext{
+			runtime:  rt,
+			function: f,
+		}
+	}
+
+	return nil
 }
 
-func (j jsProcessor) Process(ctx context.Context, records []opencdc.Record) []sdk.ProcessedRecord {
-	//TODO implement me
-	panic("implement me")
+func (p *jsProcessor) Process(_ context.Context, records []opencdc.Record) []sdk.ProcessedRecord {
+	g := p.gojaPool.Get().(*gojaContext)
+	defer p.gojaPool.Put(g)
+
+	out := make([]sdk.ProcessedRecord, len(records))
+	for i, in := range records {
+		out[i] = p.process(g, in)
+	}
+
+	return out
 }
 
-func (j jsProcessor) Teardown(ctx context.Context) error {
-	//TODO implement me
-	panic("implement me")
+func (p *jsProcessor) Teardown(context.Context) error {
+	return nil
 }
 
-func (j jsProcessor) mustEmbedUnimplementedProcessor() {
-	//TODO implement me
-	panic("implement me")
+func (p *jsProcessor) newJSRuntime(logger log.CtxLogger) (*goja.Runtime, error) {
+	rt := goja.New()
+	require.NewRegistry().Enable(rt)
+
+	runtimeHelpers := map[string]interface{}{
+		"logger":         &logger,
+		"Record":         p.jsRecord(rt),
+		"RawData":        p.jsContentRaw(rt),
+		"StructuredData": p.jsContentStructured(rt),
+	}
+
+	for name, helper := range runtimeHelpers {
+		if err := rt.Set(name, helper); err != nil {
+			return nil, cerrors.Errorf("failed to set helper %q: %w", name, err)
+		}
+	}
+
+	return rt, nil
+}
+
+func (p *jsProcessor) newFunction(runtime *goja.Runtime, src string) (goja.Callable, error) {
+	prg, err := goja.Compile("", src, false)
+	if err != nil {
+		return nil, cerrors.Errorf("failed to compile script: %w", err)
+	}
+
+	_, err = runtime.RunProgram(prg)
+	if err != nil {
+		return nil, cerrors.Errorf("failed to run program: %w", err)
+	}
+
+	tmp := runtime.Get(entrypoint)
+	entrypointFunc, ok := goja.AssertFunction(tmp)
+	if !ok {
+		return nil, cerrors.Errorf("failed to get entrypoint function %q", entrypoint)
+	}
+
+	return entrypointFunc, nil
+}
+
+func (p *jsProcessor) jsRecord(runtime *goja.Runtime) func(goja.ConstructorCall) *goja.Object {
+	return func(call goja.ConstructorCall) *goja.Object {
+		// TODO accept arguments
+		// We return a jsRecord struct, however because we are
+		// not changing call.This instanceof will not work as expected.
+
+		r := jsRecord{
+			Metadata: make(map[string]string),
+		}
+		// We need to return a pointer to make the returned object mutable.
+		return runtime.ToValue(&r).ToObject(runtime)
+	}
+}
+
+func (p *jsProcessor) jsContentRaw(runtime *goja.Runtime) func(goja.ConstructorCall) *goja.Object {
+	return func(call goja.ConstructorCall) *goja.Object {
+		// TODO accept arguments
+		// We return a record.RawData struct, however because we are
+		// not changing call.This instanceof will not work as expected.
+
+		r := record.RawData{}
+		// We need to return a pointer to make the returned object mutable.
+		return runtime.ToValue(&r).ToObject(runtime)
+	}
+}
+
+func (p *jsProcessor) jsContentStructured(runtime *goja.Runtime) func(goja.ConstructorCall) *goja.Object {
+	return func(call goja.ConstructorCall) *goja.Object {
+		// TODO accept arguments
+		// We return a map[string]interface{} struct, however because we are
+		// not changing call.This instanceof will not work as expected.
+
+		r := make(map[string]interface{})
+		return runtime.ToValue(r).ToObject(runtime)
+	}
+}
+
+func (p *jsProcessor) toJSRecord(runtime *goja.Runtime, r opencdc.Record) goja.Value {
+	convertData := func(d opencdc.Data) interface{} {
+		switch v := d.(type) {
+		case opencdc.RawData:
+			return &v
+		case opencdc.StructuredData:
+			return map[string]interface{}(v)
+		}
+		return nil
+	}
+
+	jsr := jsRecord{
+		Position:  r.Position,
+		Operation: r.Operation.String(),
+		Metadata:  r.Metadata,
+		Key:       convertData(r.Key),
+		Payload: struct {
+			Before interface{}
+			After  interface{}
+		}{
+			Before: convertData(r.Payload.Before),
+			After:  convertData(r.Payload.After),
+		},
+	}
+
+	// we need to send in a pointer to let the user change the value and return it, if they choose to do so
+	return runtime.ToValue(&jsr)
+}
+
+func (p *jsProcessor) toInternalRecord(v goja.Value) sdk.ProcessedRecord {
+	raw := v.Export()
+	if raw == nil {
+		return sdk.FilterRecord{}
+	}
+
+	jsr, ok := v.Export().(*jsRecord)
+	if !ok {
+		return sdk.ErrorRecord{
+			Error: cerrors.Errorf("js function expected to return %T, but returned: %T", &jsRecord{}, v),
+		}
+	}
+
+	var op opencdc.Operation
+	err := op.UnmarshalText([]byte(jsr.Operation))
+	if err != nil {
+		return sdk.ErrorRecord{Error: cerrors.Errorf("could not unmarshal operation: %w", err)}
+	}
+
+	convertData := func(d interface{}) opencdc.Data {
+		switch v := d.(type) {
+		case *opencdc.RawData:
+			return *v
+		case map[string]interface{}:
+			return opencdc.StructuredData(v)
+		}
+		return nil
+	}
+
+	return sdk.SingleRecord{
+		Position:  jsr.Position,
+		Operation: op,
+		Metadata:  jsr.Metadata,
+		Key:       convertData(jsr.Key),
+		Payload: opencdc.Change{
+			Before: convertData(jsr.Payload.Before),
+			After:  convertData(jsr.Payload.After),
+		},
+	}
+}
+
+func (p *jsProcessor) process(gojaCtx *gojaContext, in opencdc.Record) sdk.ProcessedRecord {
+	jsr := p.toJSRecord(gojaCtx.runtime, in)
+
+	result, err := gojaCtx.function(goja.Undefined(), jsr)
+	if err != nil {
+		return sdk.ErrorRecord{Error: cerrors.Errorf("failed to execute JS processor function: %w", err)}
+	}
+
+	return p.toInternalRecord(result)
 }
---
+++ pkg/plugin/processor/builtin/javascript_test.go
@@ -0,0 +1,473 @@
+// Copyright © 2024 Meroxa, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package builtin
+
+import (
+	"bytes"
+	"context"
+	"github.com/conduitio/conduit-commons/opencdc"
+	"github.com/conduitio/conduit/pkg/foundation/cerrors"
+	"github.com/conduitio/conduit/pkg/foundation/log"
+	"github.com/conduitio/conduit/pkg/processor"
+	"github.com/conduitio/conduit/pkg/record"
+	"github.com/dop251/goja"
+	"github.com/matryer/is"
+	"github.com/rs/zerolog"
+	"reflect"
+	"testing"
+)
+
+func TestJSProcessor_Logger(t *testing.T) {
+	is := is.New(t)
+	ctx := context.Background()
+
+	var buf bytes.Buffer
+	logger := log.New(zerolog.New(&buf))
+	underTest := newJsProcessor(logger)
+	err := underTest.Configure(
+		ctx,
+		map[string]string{
+			"script": `
+	function process(r) {
+		logger.Info().Msg("Hello");
+		return r
+	}
+	`,
+		},
+	)
+	is.NoErr(err)
+
+	err = underTest.Open(ctx)
+	is.NoErr(err)
+
+	_ = underTest.Process(context.Background(), []opencdc.Record{{}})
+
+	is.Equal(`{"level":"info","message":"Hello"}`+"\n", buf.String()) // expected different log message
+}
+
+func TestJSProcessor_MissingEntrypoint(t *testing.T) {
+	is := is.New(t)
+	ctx := context.Background()
+
+	underTest := newJsProcessor(log.Nop())
+	err := underTest.Configure(ctx, map[string]string{"script": `logger.Debug("no entrypoint");`})
+	is.NoErr(err)
+
+	err = underTest.Open(ctx)
+	is.True(err != nil)                                                                                   // expected error
+	is.Equal(`failed initializing JS function: failed to get entrypoint function "process"`, err.Error()) // expected different error message
+}
+
+func TestJSProcessor_Process(t *testing.T) {
+	type fields struct {
+		src string
+	}
+	type args struct {
+		record record.Record
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    record.Record
+		wantErr error
+	}{
+		{
+			name: "change fields of structured record",
+			fields: fields{
+				src: `
+				function process(record) {
+					record.Position = "3";
+					record.Operation = "update";
+					record.Metadata["returned"] = "JS";
+					record.Key.Raw = "baz";
+					record.Payload.After["ccc"] = "baz";
+					return record;
+				}`,
+			},
+			args: args{
+				record: record.Record{
+					Position:  []byte("2"),
+					Operation: record.OperationCreate,
+					Metadata:  record.Metadata{"existing": "val"},
+					Key:       record.RawData{Raw: []byte("bar")},
+					Payload: record.Change{
+						Before: nil,
+						After: record.StructuredData(
+							map[string]interface{}{
+								"aaa": 111,
+								"bbb": []string{"foo", "bar"},
+							},
+						),
+					},
+				},
+			},
+			want: record.Record{
+				Position:  []byte("3"),
+				Operation: record.OperationUpdate,
+				Metadata:  record.Metadata{"existing": "val", "returned": "JS"},
+				Key:       record.RawData{Raw: []byte("baz")},
+				Payload: record.Change{
+					Before: nil,
+					After: record.StructuredData(
+						map[string]interface{}{
+							"aaa": 111,
+							"bbb": []string{"foo", "bar"},
+							"ccc": "baz",
+						},
+					),
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "complete change incoming record with structured data",
+			fields: fields{
+				src: `
+				function process(record) {
+					record.Position = "3";
+					record.Metadata["returned"] = "JS";
+					record.Key.Raw = "baz";
+					record.Payload.After = new StructuredData();
+					record.Payload.After["foo"] = "bar";
+					return record;
+				}`,
+			},
+			args: args{
+				record: record.Record{
+					Position: []byte("2"),
+					Metadata: record.Metadata{"existing": "val"},
+					Key:      record.RawData{Raw: []byte("bar")},
+					Payload: record.Change{
+						Before: nil,
+						After:  record.RawData{Raw: []byte("foo")},
+					},
+				},
+			},
+			want: record.Record{
+				Position: []byte("3"),
+				Metadata: record.Metadata{"existing": "val", "returned": "JS"},
+				Key:      record.RawData{Raw: []byte("baz")},
+				Payload: record.Change{
+					Before: nil,
+					After: record.StructuredData{
+						"foo": "bar",
+					},
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "complete change incoming record with raw data",
+			fields: fields{
+				src: `
+				function process(record) {
+					record.Position = "3";
+					record.Metadata["returned"] = "JS";
+					record.Key.Raw = "baz";
+					record.Payload.After.Raw = String.fromCharCode.apply(String, record.Payload.After.Raw) + "bar";
+					return record;
+				}`,
+			},
+			args: args{
+				record: record.Record{
+					Position: []byte("2"),
+					Metadata: record.Metadata{"existing": "val"},
+					Key:      record.RawData{Raw: []byte("bar")},
+					Payload: record.Change{
+						Before: nil,
+						After:  record.RawData{Raw: []byte("foo")},
+					},
+				},
+			},
+			want: record.Record{
+				Position: []byte("3"),
+				Metadata: record.Metadata{"existing": "val", "returned": "JS"},
+				Key:      record.RawData{Raw: []byte("baz")},
+				Payload: record.Change{
+					Before: nil,
+					After:  record.RawData{Raw: []byte("foobar")},
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "return new record with raw data",
+			fields: fields{
+				src: `
+				function process(record) {
+					r = new Record();
+					r.Position = "3";
+					r.Metadata["returned"] = "JS";
+					r.Key = new RawData();
+					r.Key.Raw = "baz";
+					r.Payload.After = new RawData();
+					r.Payload.After.Raw = "foobar";
+					return r;
+				}`,
+			},
+			args: args{
+				record: record.Record{},
+			},
+			want: record.Record{
+				Position: []byte("3"),
+				Metadata: record.Metadata{"returned": "JS"},
+				Key:      record.RawData{Raw: []byte("baz")},
+				Payload: record.Change{
+					Before: nil,
+					After:  record.RawData{Raw: []byte("foobar")},
+				},
+			},
+			wantErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			is := is.New(t)
+
+			underTest, err := New(tt.fields.src, zerolog.Nop())
+			is.NoErr(err) // expected no error when creating the JS processor
+
+			got, err := underTest.Process(context.Background(), tt.args.record)
+			if tt.wantErr != nil {
+				is.Equal(tt.wantErr, err) // expected different error
+			} else {
+				is.NoErr(err) // expected no error
+			}
+
+			is.Equal(tt.want, got) // expected different record
+		})
+	}
+}
+
+func TestJSProcessor_Filtering(t *testing.T) {
+	testCases := []struct {
+		name   string
+		src    string
+		input  record.Record
+		filter bool
+	}{
+		{
+			name: "always skip",
+			src: `function process(r) {
+				return null;
+			}`,
+			input:  record.Record{},
+			filter: false,
+		},
+		{
+			name: "filter based on a field - positive",
+			src: `function process(r) {
+				if (r.Metadata["keepme"] != undefined) {
+					return r
+				}
+				return null;
+			}`,
+			input:  record.Record{Metadata: record.Metadata{"keepme": "yes"}},
+			filter: true,
+		},
+		{
+			name: "filter out based on a field - negative",
+			src: `function process(r) {
+				if (r.Metadata["keepme"] != undefined) {
+					return r
+				}
+				return null;
+			}`,
+			input:  record.Record{Metadata: record.Metadata{"foo": "bar"}},
+			filter: false,
+		},
+		{
+			name: "no return value",
+			src: `
+				function process(record) {
+					logger.Debug("no return value");
+				}`,
+			input:  record.Record{Metadata: record.Metadata{"foo": "bar"}},
+			filter: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			is := is.New(t)
+
+			underTest, err := New(tc.src, zerolog.New(zerolog.NewConsoleWriter()))
+			is.NoErr(err) // expected no error when creating the JS processor
+
+			rec, err := underTest.Process(context.Background(), tc.input)
+			if tc.filter {
+				is.NoErr(err)           // expected no error for processed record
+				is.Equal(tc.input, rec) // expected different processed record
+			} else {
+				is.True(reflect.ValueOf(rec).IsZero())            // expected zero record
+				is.True(cerrors.Is(err, processor.ErrSkipRecord)) // expected ErrSkipRecord
+			}
+		})
+	}
+}
+
+func TestJSProcessor_DataTypes(t *testing.T) {
+	testCases := []struct {
+		name  string
+		src   string
+		input record.Record
+		want  record.Record
+	}{
+		{
+			name: "position from string",
+			src: `function process(record) {
+				record.Position = "foobar";
+				return record;
+			}`,
+			input: record.Record{},
+			want: record.Record{
+				Position: record.Position("foobar"),
+			},
+		},
+		{
+			name: "raw payload, data from string",
+			src: `function process(record) {
+				record.Payload.After = new RawData();
+				record.Payload.After.Raw = "foobar";
+				return record;
+			}`,
+			input: record.Record{},
+			want: record.Record{
+				Payload: record.Change{
+					Before: nil,
+					After:  record.RawData{Raw: []byte("foobar")},
+				},
+			},
+		},
+		{
+			name: "raw key, data from string",
+			src: `function process(record) {
+				record.Key = new RawData();
+				record.Key.Raw = "foobar";
+				return record;
+			}`,
+			input: record.Record{},
+			want: record.Record{
+				Key: record.RawData{Raw: []byte("foobar")},
+			},
+		},
+		{
+			name: "update metadata",
+			src: `function process(record) {
+				record.Metadata["new_key"] = "new_value"
+				delete record.Metadata.remove_me;
+				return record;
+			}`,
+			input: record.Record{
+				Metadata: record.Metadata{
+					"old_key":   "old_value",
+					"remove_me": "remove_me",
+				},
+			},
+			want: record.Record{
+				Metadata: record.Metadata{
+					"old_key": "old_value",
+					"new_key": "new_value",
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			is := is.New(t)
+
+			underTest, err := New(tc.src, zerolog.Nop())
+			is.NoErr(err) // expected no error when creating the JS processor
+
+			got, err := underTest.Process(context.Background(), tc.input)
+			is.NoErr(err)          // expected no error when processing record
+			is.Equal(tc.want, got) // expected different record
+		})
+	}
+}
+
+func TestJSProcessor_JavaScriptException(t *testing.T) {
+	is := is.New(t)
+
+	src := `function process(record) {
+		var m;
+		m.test
+	}`
+	underTest, err := New(src, zerolog.Nop())
+	is.NoErr(err) // expected no error when creating the JS processor
+
+	r := record.Record{
+		Key: record.RawData{Raw: []byte("test key")},
+		Payload: record.Change{
+			Before: nil,
+			After:  record.RawData{Raw: []byte("test payload")},
+		},
+	}
+
+	got, err := underTest.Process(context.Background(), r)
+	is.True(err != nil) // expected error
+	target := &goja.Exception{}
+	is.True(cerrors.As(err, &target)) // expected a goja.Exception
+	is.Equal(record.Record{}, got)    // expected a zero record
+}
+
+func TestJSProcessor_BrokenJSCode(t *testing.T) {
+	is := is.New(t)
+
+	src := `function {`
+	_, err := New(src, zerolog.Nop())
+	is.True(err != nil) // expected error for invalid JS code
+	target := &goja.CompilerSyntaxError{}
+	is.True(cerrors.As(err, &target)) // expected a goja.CompilerSyntaxError
+}
+
+func TestJSProcessor_ScriptWithMultipleFunctions(t *testing.T) {
+	is := is.New(t)
+
+	src := `
+		function getValue() {
+			return "updated_value";
+		}
+		
+		function process(record) {
+			record.Metadata["updated_key"] = getValue()
+			return record;
+		}
+	`
+	underTest, err := New(src, zerolog.Nop())
+	is.NoErr(err) // expected no error when creating the JS processor
+
+	r := record.Record{
+		Metadata: record.Metadata{
+			"old_key": "old_value",
+		},
+	}
+
+	got, err := underTest.Process(context.Background(), r)
+	is.NoErr(err) // expected no error when processing record
+	is.Equal(
+		record.Record{
+			Metadata: record.Metadata{
+				"old_key":     "old_value",
+				"updated_key": "updated_value",
+			},
+		},
+		got,
+	) // expected different record
+}
