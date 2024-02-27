005995aff2690dc46f751d4097e18ee4eb906155
Haris Osmanagic <haris@meroxa.io>
2024-02-27 15:37:40 +0100

    regenerate

--- pkg/plugin/processor/builtin/unwrap_opencdc_paramgen.go
+++ pkg/plugin/processor/builtin/unwrap_opencdc_paramgen.go
@@ -13,7 +13,7 @@ func (unwrapOpenCDCConfig) Parameters() map[string]config.Parameter {
 	return map[string]config.Parameter{
 		"field": {
 			Default:     ".Payload.After",
-			Description: "Filter is a reference to the field which contains the OpenCDC record.  For more information about record references, see: https://github.com/ConduitIO/conduit-processor-sdk/blob/cbdc5dcb5d3109f8f13b88624c9e360076b0bcdb/util.go#L66.",
+			Description: "field is a reference to the field which contains the OpenCDC record.  For more information about record references, see: https://github.com/ConduitIO/conduit-processor-sdk/blob/cbdc5dcb5d3109f8f13b88624c9e360076b0bcdb/util.go#L66.",
 			Type:        config.ParameterTypeString,
 			Validations: []config.Validation{
 				config.ValidationRegex{Regex: regexp.MustCompile("^.Payload")},
