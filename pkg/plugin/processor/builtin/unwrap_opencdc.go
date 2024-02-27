4a9f09f184db2209f51824feee623edbbb570189
Haris Osmanagic <haris@meroxa.io>
2024-02-27 13:01:01 +0100

    linter

--- pkg/plugin/processor/builtin/unwrap_opencdc.go
+++ pkg/plugin/processor/builtin/unwrap_opencdc.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+
 	"github.com/conduitio/conduit-commons/config"
 	"github.com/conduitio/conduit-commons/opencdc"
 	sdk "github.com/conduitio/conduit-processor-sdk"
@@ -31,7 +32,7 @@ import (
 type unwrapOpenCDCConfig struct {
 	// Filter is a reference to the field which contains the OpenCDC record.
 	//
-	//For more information about record references, see: https://github.com/ConduitIO/conduit-processor-sdk/blob/cbdc5dcb5d3109f8f13b88624c9e360076b0bcdb/util.go#L66.
+	// For more information about record references, see: https://github.com/ConduitIO/conduit-processor-sdk/blob/cbdc5dcb5d3109f8f13b88624c9e360076b0bcdb/util.go#L66.
 	Field string `json:"field" validate:"regex=^.Payload" default:".Payload.After"`
 }
 
