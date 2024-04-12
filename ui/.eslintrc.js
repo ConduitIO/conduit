4c29167bf84891917922ba986aa0a95291afc702
Raúl Barroso <ra.barroso@gmail.com>
2024-04-12 15:24:23 +0200

    leave a comment

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -27,6 +27,8 @@ module.exports = {
     // this can be removed once the following is fixed
     // https://github.com/mysticatea/eslint-plugin-node/issues/77
     'node/no-unpublished-require': 'off',
+
+    // TODO: remove this and fix issues raised
     'ember/no-runloop': 'off',
   },
   overrides: [
