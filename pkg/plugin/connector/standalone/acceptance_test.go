5d0bc57c44af62251449aae5aa4619d2ee710ca0
Haris Osmanagic <haris@meroxa.io>
2024-08-21 18:42:49 +0200

    linter

--- pkg/plugin/connector/standalone/acceptance_test.go
+++ pkg/plugin/connector/standalone/acceptance_test.go
@@ -18,8 +18,7 @@ import (
 	"testing"
 
 	"github.com/conduitio/conduit-connector-protocol/pconnector/mock"
-	//nolint:staticcheck // backwards compatibility
-	v1 "github.com/conduitio/conduit-connector-protocol/pconnector/v1"
+	v1 "github.com/conduitio/conduit-connector-protocol/pconnector/v1" //nolint:staticcheck // backwards compatibility
 	v2 "github.com/conduitio/conduit-connector-protocol/pconnector/v2"
 	"github.com/conduitio/conduit/pkg/plugin/connector"
 	"github.com/rs/zerolog"
