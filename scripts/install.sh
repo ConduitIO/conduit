d0ad26ea602221c9553754dfb98044d4a5669f50
Haris Osmanagic <haris@meroxa.io>
2024-10-17 14:47:33 +0200

    copyright

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Copyright The Helm Authors.
+# Copyright © 2024 Meroxa, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
