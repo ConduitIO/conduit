1068050f14dd587a3fbf42ef498b96b766ea7694
Haris Osmanagic <haris@meroxa.io>
2024-11-04 15:14:13 +0100

    update test

--- pkg/connector/service_test.go
+++ pkg/connector/service_test.go
@@ -514,6 +514,7 @@ func TestService_UpdateSuccess(t *testing.T) {
 		Name:     "changed-name",
 		Settings: map[string]string{"foo": "bar"},
 	}
+	wantPlugin := "changed-plugin"
 
 	conn, err := service.Create(
 		ctx,
@@ -530,10 +531,11 @@ func TestService_UpdateSuccess(t *testing.T) {
 	is.NoErr(err)
 
 	beforeUpdate := time.Now()
-	got, err := service.Update(ctx, conn.ID, "test-plugin", want)
+	got, err := service.Update(ctx, conn.ID, wantPlugin, want)
 	is.NoErr(err)
 
 	is.Equal(got.Config, want)
+	is.Equal(got.Plugin, wantPlugin)
 	is.True(!got.UpdatedAt.Before(beforeUpdate))
 }
 
