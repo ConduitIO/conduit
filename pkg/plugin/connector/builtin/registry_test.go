aa172442c6cccc046329dfbf8a0fba8cd4e99bee
Haris Osmanagic <haris@meroxa.io>
2024-11-12 11:42:48 +0100

    builtin connector registry test

--- pkg/plugin/connector/builtin/registry_test.go
+++ pkg/plugin/connector/builtin/registry_test.go
@@ -17,7 +17,9 @@ package builtin
 import (
 	"context"
 	"github.com/conduitio/conduit-connector-protocol/pconnector"
+	"github.com/conduitio/conduit/pkg/foundation/cerrors"
 	"github.com/conduitio/conduit/pkg/foundation/log"
+	"github.com/conduitio/conduit/pkg/plugin"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"testing"
@@ -30,8 +32,8 @@ func TestRegistry_InitList(t *testing.T) {
 	ctx := context.Background()
 
 	underTest := NewRegistry(log.Nop(), DefaultBuiltinConnectors, nil)
-
 	underTest.Init(ctx)
+
 	specs := underTest.List()
 
 	is.Equal(len(DefaultBuiltinConnectors), len(specs))
@@ -46,4 +48,64 @@ func TestRegistry_InitList(t *testing.T) {
 			),
 		)
 	}
+}
+
+func TestRegistry_NewDispenser_PluginNotFound(t *testing.T) {
+	testCases := []struct {
+		name       string
+		pluginName string
+		wantErr    bool
+	}{
+		{
+			name:       "non-existing plugin",
+			pluginName: "builtin:foobar",
+			wantErr:    true,
+		},
+		{
+			name:       "plugin exists, version doesn't",
+			pluginName: "builtin:file@v12.34.56",
+			wantErr:    true,
+		},
+		{
+			name:       "existing plugin, no builtin prefix, no version",
+			pluginName: "file",
+			wantErr:    false,
+		},
+		{
+			name:       "existing plugin, with builtin prefix, no version",
+			pluginName: "builtin:file",
+			wantErr:    false,
+		},
+		{
+			name: "existing plugin, with builtin prefix, with version",
+			pluginName: func() string {
+				v := DefaultBuiltinConnectors["github.com/conduitio/conduit-connector-file"].
+					NewSpecification().
+					Version
+				return "builtin:file@" + v
+			}(),
+			wantErr: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			is := is.New(t)
+			ctx := context.Background()
+
+			underTest := NewRegistry(log.Nop(), DefaultBuiltinConnectors, nil)
+			underTest.Init(ctx)
+
+			dispenser, err := underTest.NewDispenser(log.Nop(), plugin.FullName(tc.pluginName), pconnector.PluginConfig{})
+			if tc.wantErr {
+				is.True(cerrors.Is(err, plugin.ErrPluginNotFound))
+				is.True(dispenser == nil)
+				return
+			}
+
+			is.NoErr(err)
+			is.True(dispenser != nil)
+		})
+	}
+
 }
