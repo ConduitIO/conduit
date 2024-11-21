790e0052ac3c880861de116a01857a5ac8c16a62
Raúl Barroso <ra.barroso@gmail.com>
2024-11-21 16:13:54 +0100

    update which ones are general flags

--- cmd/conduit/root/root.go
+++ cmd/conduit/root/root.go
@@ -34,46 +34,46 @@ var (
 // TODO: Check which ones are really global from the design document
 type RootFlags struct {
 	// Database configuration
-	DBType                     string `long:"db.type" usage:"database type; accepts badger,postgres,inmemory,sqlite" persistent:"true"`
-	DBBadgerPath               string `long:"db.badger.path" usage:"path to badger DB" persistent:"true"`
-	DBPostgresConnectionString string `long:"db.postgres.connection-string" usage:"postgres connection string, may be a database URL or in PostgreSQL keyword/value format" persistent:"true"`
-	DBPostgresTable            string `long:"db.postgres.table" usage:"postgres table in which to store data (will be created if it does not exist)" persistent:"true"`
-	DBSQLitePath               string `long:"db.sqlite.path" usage:"path to sqlite3 DB" persistent:"true"`
-	DBSQLiteTable              string `long:"db.sqlite.table" usage:"sqlite3 table in which to store data (will be created if it does not exist)" persistent:"true"`
+	DBType                     string `long:"db.type" usage:"database type; accepts badger,postgres,inmemory,sqlite"`
+	DBBadgerPath               string `long:"db.badger.path" usage:"path to badger DB"`
+	DBPostgresConnectionString string `long:"db.postgres.connection-string" usage:"postgres connection string, may be a database URL or in PostgreSQL keyword/value format"`
+	DBPostgresTable            string `long:"db.postgres.table" usage:"postgres table in which to store data (will be created if it does not exist)"`
+	DBSQLitePath               string `long:"db.sqlite.path" usage:"path to sqlite3 DB"`
+	DBSQLiteTable              string `long:"db.sqlite.table" usage:"sqlite3 table in which to store data (will be created if it does not exist)"`
 
 	// API configuration
-	APIEnabled     bool   `long:"api.enabled" usage:"enable HTTP and gRPC API" persistent:"true"`
-	APIHTTPAddress string `long:"http.address" usage:"address for serving the HTTP API" persistent:"true"`
-	APIGRPCAddress string `long:"grpc.address" usage:"address for serving the gRPC API" persistent:"true"`
+	APIEnabled     bool   `long:"api.enabled" usage:"enable HTTP and gRPC API"`
+	APIHTTPAddress string `long:"http.address" usage:"address for serving the HTTP API"`
+	APIGRPCAddress string `long:"grpc.address" usage:"address for serving the gRPC API"`
 
 	// Logging configuration
-	LogLevel  string `long:"log.level" usage:"sets logging level; accepts debug, info, warn, error, trace" persistent:"true"`
-	LogFormat string `long:"log.format" usage:"sets the format of the logging; accepts json, cli" persistent:"true"`
+	LogLevel  string `long:"log.level" usage:"sets logging level; accepts debug, info, warn, error, trace"`
+	LogFormat string `long:"log.format" usage:"sets the format of the logging; accepts json, cli"`
 
 	// Connectors and Processors paths
-	ConnectorsPath string `long:"connectors.path" usage:"path to standalone connectors' directory" persistent:"true"`
-	ProcessorsPath string `long:"processors.path" usage:"path to standalone processors' directory" persistent:"true"`
+	ConnectorsPath string `long:"connectors.path" usage:"path to standalone connectors' directory"`
+	ProcessorsPath string `long:"processors.path" usage:"path to standalone processors' directory"`
 
 	// Pipeline configuration
-	PipelinesPath                          string        `long:"pipelines.path" usage:"path to the directory that has the yaml pipeline configuration files, or a single pipeline configuration file" persistent:"true"`
-	PipelinesExitOnDegraded                bool          `long:"pipelines.exit-on-degraded" usage:"exit Conduit if a pipeline enters a degraded state" persistent:"true"`
-	PipelinesErrorRecoveryMinDelay         time.Duration `long:"pipelines.error-recovery.min-delay" usage:"minimum delay before restart" persistent:"true"`
-	PipelinesErrorRecoveryMaxDelay         time.Duration `long:"pipelines.error-recovery.max-delay" usage:"maximum delay before restart" persistent:"true"`
-	PipelinesErrorRecoveryBackoffFactor    int           `long:"pipelines.error-recovery.backoff-factor" usage:"backoff factor applied to the last delay" persistent:"true"`
-	PipelinesErrorRecoveryMaxRetries       int64         `long:"pipelines.error-recovery.max-retries" usage:"maximum number of retries" persistent:"true"`
-	PipelinesErrorRecoveryMaxRetriesWindow time.Duration `long:"pipelines.error-recovery.max-retries-window" usage:"amount of time running without any errors after which a pipeline is considered healthy" persistent:"true"`
+	PipelinesPath                          string        `long:"pipelines.path" usage:"path to the directory that has the yaml pipeline configuration files, or a single pipeline configuration file"`
+	PipelinesExitOnDegraded                bool          `long:"pipelines.exit-on-degraded" usage:"exit Conduit if a pipeline enters a degraded state"`
+	PipelinesErrorRecoveryMinDelay         time.Duration `long:"pipelines.error-recovery.min-delay" usage:"minimum delay before restart"`
+	PipelinesErrorRecoveryMaxDelay         time.Duration `long:"pipelines.error-recovery.max-delay" usage:"maximum delay before restart"`
+	PipelinesErrorRecoveryBackoffFactor    int           `long:"pipelines.error-recovery.backoff-factor" usage:"backoff factor applied to the last delay"`
+	PipelinesErrorRecoveryMaxRetries       int64         `long:"pipelines.error-recovery.max-retries" usage:"maximum number of retries"`
+	PipelinesErrorRecoveryMaxRetriesWindow time.Duration `long:"pipelines.error-recovery.max-retries-window" usage:"amount of time running without any errors after which a pipeline is considered healthy"`
 
 	// Schema registry configuration
-	SchemaRegistryType                      string `long:"schema-registry.type" usage:"schema registry type; accepts builtin,confluent" persistent:"true"`
-	SchemaRegistryConfluentConnectionString string `long:"schema-registry.confluent.connection-string" usage:"confluent schema registry connection string" persistent:"true"`
+	SchemaRegistryType                      string `long:"schema-registry.type" usage:"schema registry type; accepts builtin,confluent"`
+	SchemaRegistryConfluentConnectionString string `long:"schema-registry.confluent.connection-string" usage:"confluent schema registry connection string"`
 
 	// Preview features
-	PreviewPipelineArchV2 bool `long:"preview.pipeline-arch-v2" usage:"enables experimental pipeline architecture v2 (note that the new architecture currently supports only 1 source and 1 destination per pipeline)" persistent:"true"`
+	PreviewPipelineArchV2 bool `long:"preview.pipeline-arch-v2" usage:"enables experimental pipeline architecture v2 (note that the new architecture currently supports only 1 source and 1 destination per pipeline)"`
 
 	// Development profiling
-	DevCPUProfile   string `long:"dev.cpuprofile" usage:"write CPU profile to file" persistent:"true"`
-	DevMemProfile   string `long:"dev.memprofile" usage:"write memory profile to file" persistent:"true"`
-	DevBlockProfile string `long:"dev.blockprofile" usage:"write block profile to file" persistent:"true"`
+	DevCPUProfile   string `long:"dev.cpuprofile" usage:"write CPU profile to file"`
+	DevMemProfile   string `long:"dev.memprofile" usage:"write memory profile to file"`
+	DevBlockProfile string `long:"dev.blockprofile" usage:"write block profile to file"`
 
 	// Version
 	Version bool `long:"version" short:"v" usage:"show version" persistent:"true"`
