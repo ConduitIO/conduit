87006cfe2d36b79fe7c41202f70aa90d417e31cd
Raúl Barroso <ra.barroso@gmail.com>
2024-12-19 17:45:45 +0100

    refactor: remove UI

--- ui/app/helpers/add-one.js
+++
@@ -1,9 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-function addOne(args) {
-  let [index] = args;
-
-  return index + 1;
-}
-
-export default helper(addOne);
--- ui/app/helpers/subtract.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-function subtract([x, y]) {
-  return x - y;
-}
-
-export default helper(subtract);
