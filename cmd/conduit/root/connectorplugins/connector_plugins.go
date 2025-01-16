0e58602faef311ffae57c49db3349b30b2ccd173
Raúl Barroso <ra.barroso@gmail.com>
2025-01-16 19:23:29 +0100

    fix pkg name

--- cmd/conduit/root/connectorplugins/connector_plugins.go
+++ cmd/conduit/root/connectorplugins/connector_plugins.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package connector_plugins
+package connectorplugins
 
 import (
 	"github.com/conduitio/ecdysis"
