6b93077f42d5d9f3a40aab50356a97b4092de2a7
Guillem <someemail@dot.com>
2025-02-25 22:38:57 +0700

    fix lint

--- pkg/plugin/processor/builtin/impl/openai-textgen/textgen_test.go
+++ pkg/plugin/processor/builtin/impl/openai-textgen/textgen_test.go
@@ -1,9 +1,23 @@
+// Copyright © 2024 Meroxa, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package textgen
 
 import (
 	"context"
-	"os"
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/conduitio/conduit-commons/config"
@@ -39,19 +53,17 @@ func TestProcessor_Process(t *testing.T) {
 }
 
 func newProcessor(ctx context.Context, is *is.I, devMessage string) sdk.Processor {
-	processor := NewProcessor()
-
-	apikey := os.Getenv("OPENAI_API_KEY")
-	is.True(apikey != "") // OPENAI_API_KEY must be set
+	processor := &Processor{}
 
 	cfg := config.Config{
 		ProcessorConfigModel:            openai.GPT4oMini,
-		ProcessorConfigApiKey:           apikey,
+		ProcessorConfigApiKey:           "fake api key",
 		ProcessorConfigDeveloperMessage: devMessage,
 		ProcessorConfigTemperature:      "0",
 	}
 
 	is.NoErr(processor.Configure(ctx, cfg))
+	processor.call = fakeOpenaiCall{}
 
 	return processor
 }
@@ -83,4 +95,10 @@ func testRecords() []opencdc.Record {
 			},
 		},
 	}
+}
+
+type fakeOpenaiCall struct{}
+
+func (f fakeOpenaiCall) Call(ctx context.Context, input string) (string, error) {
+	return strings.ToUpper(input), nil
 }
