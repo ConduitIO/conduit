ce4f6f45343bd3e7de1c47c83555fe834d118025
Sarah Sicard <18204584+ssicard@users.noreply.github.com>
2025-04-29 08:04:03 -0700

    Add Ollama Processor (#2227)
    * initial commit
    
    * fix structured data response
    
    * turn off stream response from ollama
    
    * centralize logging, clean up comments
    
    * fix request log
    
    * add input validation
    
    * rename vars
    
    * make ref field configurable
    
    * add unit tests for ollama processor
    
    * remove comment
    
    * remove comment
    
    * fix ollama constructor name
    
    * use uber mock rather than go mock, reformat config test
    
    * add bodyclose explanation
    
    * clean gomod
    
    * Update pkg/plugin/processor/builtin/impl/ollama/ollama.go
    
    Co-authored-by: Maha M <82542081+maha-hajja@users.noreply.github.com>
    
    * Update pkg/plugin/processor/builtin/impl/ollama/ollama.go
    
    Co-authored-by: Maha M <82542081+maha-hajja@users.noreply.github.com>
    
    * Update pkg/plugin/processor/builtin/impl/ollama/ollama.go
    
    Co-authored-by: Maha M <82542081+maha-hajja@users.noreply.github.com>
    
    * Update pkg/plugin/processor/builtin/impl/ollama/ollama.go
    
    Co-authored-by: Maha M <82542081+maha-hajja@users.noreply.github.com>
    
    * Update pkg/plugin/processor/builtin/impl/ollama/ollama_test.go
    
    Co-authored-by: Maha M <82542081+maha-hajja@users.noreply.github.com>
    
    * change model prohibited message to be more specific
    
    * Update pkg/plugin/processor/builtin/impl/ollama/ollama.go
    
    Co-authored-by: Maha M <82542081+maha-hajja@users.noreply.github.com>
    
    * update desc for processor
    
    * fix tests
    
    * tidy go mod
    
    * change log level for record to trace
    
    ---------
    
    Co-authored-by: Maha M <82542081+maha-hajja@users.noreply.github.com>

---
+++ pkg/plugin/processor/builtin/impl/ollama/ollama.go
@@ -0,0 +1,307 @@
+// Copyright © 2025 Meroxa, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//go:generate paramgen -output=paramgen_command.go ollamaProcessorConfig
+//go:generate go run go.uber.org/mock/mockgen  --build_flags=--mod=mod -source=./ollama.go -destination=mock/http_client_mock.go -package=mock
+
+package ollama
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"slices"
+	"strings"
+
+	"github.com/conduitio/conduit-commons/config"
+	"github.com/conduitio/conduit-commons/opencdc"
+	sdk "github.com/conduitio/conduit-processor-sdk"
+	"github.com/conduitio/conduit/pkg/foundation/cerrors"
+	"github.com/conduitio/conduit/pkg/foundation/log"
+	"github.com/goccy/go-json"
+	"github.com/rs/zerolog"
+)
+
+var HTTPClient httpClient = http.DefaultClient
+
+type httpClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+// limiting to llama3.2 for MVP
+var allowedModels = []string{
+	"llama3.2",
+}
+
+type ollamaProcessorConfig struct {
+	// Field is the reference to the field to process. Defaults to ".Payload.After".
+	Field string `json:"field" validate:"regex=^.Payload" default:".Payload.After"`
+	// OllamaURL is the url to the ollama instance
+	OllamaURL string `json:"url" validate:"required"`
+	// Model is the name of the model used with ollama
+	Model string `json:"model" default:"llama3.2"`
+	// Prompt is the prompt to pass into ollama to tranform the data
+	Prompt string `json:"prompt"`
+}
+
+type ollamaProcessor struct {
+	sdk.UnimplementedProcessor
+
+	config      ollamaProcessorConfig
+	logger      log.CtxLogger
+	fieldRefRes sdk.ReferenceResolver
+}
+
+type OllamaResponse struct {
+	Response string `json:"response"`
+	Done     bool   `json:"done"`
+}
+
+func NewOllamaProcessor(l log.CtxLogger) sdk.Processor {
+	return &ollamaProcessor{logger: l.WithComponent("ollama")}
+}
+
+func (p *ollamaProcessor) Configure(ctx context.Context, cfg config.Config) error {
+	err := sdk.ParseConfig(ctx, cfg, &p.config, ollamaProcessorConfig{}.Parameters())
+	if err != nil {
+		return fmt.Errorf("failed to parse configuration: %w", err)
+	}
+
+	p.fieldRefRes, err = sdk.NewReferenceResolver(p.config.Field)
+	if err != nil {
+		return cerrors.Errorf("failed to create reference resolver: %w", err)
+	}
+
+	return nil
+}
+
+func (p *ollamaProcessor) Specification() (sdk.Specification, error) {
+	return sdk.Specification{
+		Name:        "ollama",
+		Summary:     "Processes data through an ollama instance",
+		Description: "This processor runs provided records through a specified model on a provided ollama instance. It uses the provided prompt to transform the incoming records.",
+		Version:     "v0.0.1",
+		Author:      "Meroxa, Inc.",
+		Parameters:  p.config.Parameters(),
+	}, nil
+}
+
+func (p *ollamaProcessor) Process(ctx context.Context, records []opencdc.Record) []sdk.ProcessedRecord {
+	logger := sdk.Logger(ctx)
+	processedRecords := make([]sdk.ProcessedRecord, 0, len(records))
+
+	if !slices.Contains(allowedModels, p.config.Model) {
+		return append(processedRecords, sdk.ErrorRecord{Error: fmt.Errorf("model {%s} not allowed by processor. Allowed models: %v", p.config.Model, allowedModels)})
+	}
+
+	for _, rec := range records {
+		processedRecord, err := p.processRecord(ctx, rec, logger)
+		if err != nil {
+			return append(processedRecords, sdk.ErrorRecord{Error: err})
+		}
+
+		processedRecords = append(processedRecords, processedRecord)
+	}
+
+	return processedRecords
+}
+
+func (p *ollamaProcessor) processRecord(ctx context.Context, rec opencdc.Record, logger *zerolog.Logger) (sdk.ProcessedRecord, error) {
+	ref, err := p.fieldRefRes.Resolve(&rec)
+	if err != nil {
+		return nil, cerrors.Errorf("failed resolving reference: %w", err)
+	}
+
+	field := ref.Get()
+	data, err := p.structuredData(field)
+	if err != nil {
+		return nil, cerrors.Errorf("cannot process field: %w", err)
+	}
+
+	reqBody, err := p.constructOllamaRequest(data)
+	if err != nil {
+		return nil, cerrors.Errorf("creating the ollama request %w", err)
+	}
+	logger.Trace().Msg(fmt.Sprintf("Ollama Request: %s", reqBody))
+
+	resp, err := p.sendOllamaRequest(ctx, reqBody)
+	if err != nil {
+		return nil, cerrors.Errorf("error sending the ollama request %s", err)
+	}
+
+	respData, err := processOllamaResponse(resp)
+	if err != nil {
+		return nil, cerrors.Errorf("cannot process response: %w", err)
+	}
+
+	err = ref.Set(respData)
+	if err != nil {
+		return nil, cerrors.Errorf("error setting the designated field %w", err)
+	}
+
+	return sdk.SingleRecord(rec), nil
+}
+
+func (p *ollamaProcessor) constructOllamaRequest(rec opencdc.StructuredData) ([]byte, error) {
+	prompt, err := generatePrompt(p.config.Prompt, rec)
+	if err != nil {
+		return nil, err
+	}
+
+	data := map[string]interface{}{
+		"model":  p.config.Model,
+		"prompt": prompt,
+		"format": "json",
+		"stream": false,
+	}
+	reqBody, err := json.Marshal(data)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling json %w", err)
+	}
+	return reqBody, nil
+}
+
+func (p *ollamaProcessor) sendOllamaRequest(ctx context.Context, reqBody []byte) ([]byte, error) {
+	baseURL := fmt.Sprintf("%s/api/generate", p.config.OllamaURL)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, baseURL, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return nil, fmt.Errorf("unable to create ollama request %w", err)
+	}
+
+	resp, err := HTTPClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("request to ollama failed %w", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("ollama request returned status code %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading body of call %w", err)
+	}
+
+	return body, nil
+}
+
+func generatePrompt(userPrompt string, record opencdc.StructuredData) (string, error) {
+	conduitInstructions := "For the prompt, return a valid json following the instructions provided. Only send back records in the json format with no explanation."
+	prompt := fmt.Sprintf(
+		"Instructions: {%s}\n Record: {%s} \n Suffix {%s}",
+		userPrompt,
+		record,
+		conduitInstructions)
+
+	err := validatePrompt(prompt)
+	if err != nil {
+		return "", fmt.Errorf("prompt validation failed %w", err)
+	}
+
+	return prompt, nil
+}
+
+// processOllamaResponse parses the response from the ollama call and returns only the desired response
+// as opencdc.StructuredData
+func processOllamaResponse(body []byte) (opencdc.StructuredData, error) {
+	var response OllamaResponse
+	err := json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, fmt.Errorf("invalid JSON in ollama response: %w", err)
+	}
+
+	if !response.Done {
+		return nil, fmt.Errorf("error response from ollama %w", err)
+	}
+
+	var returnData opencdc.StructuredData
+	err = json.Unmarshal([]byte(response.Response), &returnData)
+	if err != nil {
+		return nil, fmt.Errorf("invalid JSON in response: %w", err)
+	}
+
+	return returnData, nil
+}
+
+type PromptValidationConfig struct {
+	MaxLength       int
+	MinLength       int
+	BlockedPatterns []string
+}
+
+// validatePrompt will perform some security checks against the user input
+func validatePrompt(input string) error {
+	config := PromptValidationConfig{
+		MaxLength: 4096,
+		MinLength: 3,
+		BlockedPatterns: []string{
+			"rm -rf",
+			"DROP TABLE",
+			"<script>",
+			"javascript:",
+			"data:text/html",
+		},
+	}
+
+	if len(input) < config.MinLength {
+		return fmt.Errorf("prompt with length %d is too short, must be %d characters", len(input), config.MinLength)
+	}
+
+	if len(input) > config.MaxLength {
+		return fmt.Errorf("prompt with length %d exceeds context window allowance, must be less than %d characters", len(input), config.MaxLength)
+	}
+
+	for _, pattern := range config.BlockedPatterns {
+		if strings.Contains(strings.ToLower(input), strings.ToLower(pattern)) {
+			return fmt.Errorf("potentially malicious pattern: %s", pattern)
+		}
+	}
+
+	return nil
+}
+
+func (p *ollamaProcessor) structuredData(data any) (opencdc.StructuredData, error) {
+	var sd opencdc.StructuredData
+	switch v := data.(type) {
+	case opencdc.RawData:
+		b := v.Bytes()
+		// if data is empty, then return empty structured data
+		if len(b) == 0 {
+			return sd, nil
+		}
+		err := json.Unmarshal(b, &sd)
+		if err != nil {
+			return nil, cerrors.Errorf("failed unmarshalling JSON from raw data: %w", err)
+		}
+	case string:
+		err := json.Unmarshal([]byte(v), &sd)
+		if err != nil {
+			return nil, cerrors.Errorf("failed unmarshalling JSON from string: %w", err)
+		}
+	case []byte:
+		err := json.Unmarshal(v, &sd)
+		if err != nil {
+			return nil, cerrors.Errorf("failed unmarshalling JSON from []byte: %w", err)
+		}
+	case opencdc.StructuredData:
+		sd = v
+	case nil:
+		return nil, cerrors.Errorf("field to unmarshal nil")
+	default:
+		return nil, cerrors.Errorf("unexpected data type %T", v)
+	}
+
+	return sd, nil
+}
---
+++ pkg/plugin/processor/builtin/impl/ollama/ollama_test.go
@@ -0,0 +1,326 @@
+// Copyright © 2025 Meroxa, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ollama
+
+import (
+	"context"
+	_ "embed"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/conduitio/conduit-commons/config"
+	"github.com/conduitio/conduit-commons/opencdc"
+	sdk "github.com/conduitio/conduit-processor-sdk"
+	"github.com/conduitio/conduit/pkg/foundation/cerrors"
+	"github.com/conduitio/conduit/pkg/foundation/log"
+	"github.com/conduitio/conduit/pkg/plugin/processor/builtin/impl/ollama/mock"
+	"github.com/conduitio/conduit/pkg/plugin/processor/builtin/internal"
+	"github.com/google/go-cmp/cmp"
+	"github.com/matryer/is"
+	"go.uber.org/mock/gomock"
+)
+
+//go:embed test/ollama-response.json
+var ollamaResp string
+
+const ollamaURL = "http://localhost:11434"
+
+func TestOllamaProcessor_Configure(t *testing.T) {
+	ctx := context.Background()
+
+	testCases := []struct {
+		name    string
+		config  config.Config
+		wantErr error
+	}{
+		{
+			name: "applies set params",
+			config: config.Config{
+				ollamaProcessorConfigModel:  "llama3.2",
+				ollamaProcessorConfigPrompt: "hello world",
+				ollamaProcessorConfigUrl:    ollamaURL,
+				ollamaProcessorConfigField:  ".Payload.After",
+			},
+		},
+		{
+			name:    "errors with no required param",
+			config:  config.Config{},
+			wantErr: cerrors.New("failed to parse configuration: config invalid: error validating \"url\": required parameter is not provided"),
+		},
+		{
+			name: "invalid field pass through",
+			config: config.Config{
+				ollamaProcessorConfigUrl:   ollamaURL,
+				ollamaProcessorConfigField: ".Payload.Atfer",
+			},
+			wantErr: cerrors.New("failed to create reference resolver: invalid reference \".Payload.Atfer\": unexpected field \"Atfer\": cannot resolve reference"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			is := is.New(t)
+			p := &ollamaProcessor{}
+			cfg := tc.config
+
+			err := p.Configure(ctx, cfg)
+
+			if tc.wantErr == nil {
+				is.NoErr(err)
+			} else {
+				is.True(err != nil)
+				is.Equal(tc.wantErr.Error(), err.Error())
+			}
+		})
+	}
+}
+
+//nolint:bodyclose // This is a false positive, the close occurs in the ollama file
+func TestOllamaProcessor_Success(t *testing.T) {
+	ctx := context.Background()
+	ollamaURL := "http://localhost:11434"
+
+	testCases := []struct {
+		name    string
+		setup   func() config.Config
+		records []opencdc.Record
+		want    []sdk.ProcessedRecord
+	}{
+		{
+			name: "successfully processes record",
+			setup: func() config.Config {
+				client := setupHTTPMockClient(t)
+				resp := getOllamaResp(t)
+				client.EXPECT().Do(gomock.Any()).DoAndReturn(resp)
+
+				return config.Config{
+					ollamaProcessorConfigUrl: ollamaURL,
+				}
+			},
+			records: []opencdc.Record{
+				{
+					Payload: opencdc.Change{
+						After: opencdc.StructuredData{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+			want: []sdk.ProcessedRecord{
+				sdk.SingleRecord{
+					Payload: opencdc.Change{
+						After: opencdc.StructuredData{
+							"hello": "world",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "uses custom field",
+			setup: func() config.Config {
+				client := setupHTTPMockClient(t)
+				resp := getOllamaResp(t)
+				client.EXPECT().Do(gomock.Any()).DoAndReturn(resp)
+
+				return config.Config{
+					ollamaProcessorConfigModel:  "llama3.2",
+					ollamaProcessorConfigField:  ".Payload.Before",
+					ollamaProcessorConfigPrompt: "hello world",
+					ollamaProcessorConfigUrl:    "http://localhost:11434",
+				}
+			},
+			records: []opencdc.Record{
+				{
+					Payload: opencdc.Change{
+						Before: opencdc.StructuredData{
+							"test1": "test2",
+						},
+						After: opencdc.StructuredData{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+			want: []sdk.ProcessedRecord{
+				sdk.SingleRecord{
+					Payload: opencdc.Change{
+						Before: opencdc.StructuredData{
+							"hello": "world",
+						},
+						After: opencdc.StructuredData{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			is := is.New(t)
+			config := tc.setup()
+			p := NewOllamaProcessor(log.Nop())
+			err := p.Configure(ctx, config)
+			is.NoErr(err)
+
+			got := p.Process(ctx, tc.records)
+			is.Equal(len(tc.records), len(got))
+
+			for i, rec := range got {
+				_, isError := rec.(sdk.ErrorRecord)
+				is.Equal(isError, false)
+
+				processed, ok := rec.(sdk.SingleRecord)
+				is.True(ok)
+
+				is.Equal(processed, tc.want[i])
+			}
+		})
+	}
+}
+
+func TestOllamaProcessor_Failure(t *testing.T) {
+	ctx := context.Background()
+	ollamaURL := "http://localhost:11434"
+
+	testcases := []struct {
+		name    string
+		setup   func() config.Config
+		records []opencdc.Record
+		wantErr error
+	}{
+		{
+			name: "error occurs from ollama connection",
+			setup: func() config.Config {
+				client := setupHTTPMockClient(t)
+				respFn := func(_ *http.Request) (*http.Response, error) {
+					resp := &http.Response{
+						StatusCode: 500,
+						Body:       io.NopCloser(strings.NewReader("Couldn't connect")),
+					}
+					return resp, nil
+				}
+				client.EXPECT().Do(gomock.Any()).DoAndReturn(respFn)
+
+				return config.Config{
+					ollamaProcessorConfigUrl: ollamaURL,
+				}
+			},
+			records: []opencdc.Record{
+				{
+					Payload: opencdc.Change{
+						After: opencdc.StructuredData{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+			wantErr: cerrors.New("error sending the ollama request ollama request returned status code 500"),
+		},
+		{
+			name: "invalid model pass through",
+			setup: func() config.Config {
+				return config.Config{
+					ollamaProcessorConfigUrl:   ollamaURL,
+					ollamaProcessorConfigModel: "chatgpt",
+				}
+			},
+			records: []opencdc.Record{
+				{
+					Payload: opencdc.Change{
+						After: opencdc.StructuredData{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+			wantErr: cerrors.New("model {chatgpt} not allowed by processor. Allowed models: [llama3.2]"),
+		},
+		{
+			name: "bad format returned from ollama",
+			setup: func() config.Config {
+				client := setupHTTPMockClient(t)
+				respFn := func(_ *http.Request) (*http.Response, error) {
+					resp := &http.Response{
+						StatusCode: 200,
+						Body:       io.NopCloser(strings.NewReader("hello world")),
+					}
+					return resp, nil
+				}
+				client.EXPECT().Do(gomock.Any()).DoAndReturn(respFn)
+
+				return config.Config{
+					ollamaProcessorConfigUrl: ollamaURL,
+				}
+			},
+			records: []opencdc.Record{
+				{
+					Payload: opencdc.Change{
+						After: opencdc.StructuredData{
+							"foo": "bar",
+						},
+					},
+				},
+			},
+			wantErr: cerrors.New("cannot process response: invalid JSON in ollama response: invalid character 'h' looking for beginning of value"),
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			is := is.New(t)
+			config := tc.setup()
+			p := NewOllamaProcessor(log.Nop())
+			err := p.Configure(ctx, config)
+			is.NoErr(err)
+
+			result := p.Process(ctx, tc.records)
+
+			for _, rec := range result {
+				is.Equal("", cmp.Diff(sdk.ErrorRecord{Error: tc.wantErr}, rec, internal.CmpProcessedRecordOpts...))
+			}
+		})
+	}
+}
+
+func setupHTTPMockClient(t *testing.T) *mock.MockhttpClient {
+	t.Helper()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockClient := mock.NewMockhttpClient(ctrl)
+	HTTPClient = mockClient
+
+	return mockClient
+}
+
+func getOllamaResp(t *testing.T) func(*http.Request) (*http.Response, error) {
+	t.Helper()
+
+	respFn := func(_ *http.Request) (*http.Response, error) {
+		resp := &http.Response{
+			StatusCode: 200,
+			Body:       io.NopCloser(strings.NewReader(ollamaResp)),
+		}
+		return resp, nil
+	}
+
+	return respFn
+}
